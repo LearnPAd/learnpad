09c462aa6e30dc289e0c48f0f542eaad85c76536
Jean SIMARD <woshilapin@gmail.com>
2014-12-23 14:54:10 +0100

    Add WAR and XAR (first build)

---
+++ lp-collaborative-workspace/lp-collaborative-workspace-application/src/xwiki/this.js
@@ -0,0 +1,6 @@
+Package(function (pkg, XWiki) {
+    pkg.setXarName('lp-collaborative-workspace-application.xar');
+    pkg.setName("Learn PAd - Collaborative Workspace - Application");
+    pkg.setDescription("The Learn PAd application that can display stored models");
+    pkg.setExtensionId("com.xwikisas:lp-collaborative-workspace-application");
+});
