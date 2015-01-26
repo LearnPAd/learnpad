09b93687426601424e7c127550da47e81814dfaa
Jean SIMARD <woshilapin@gmail.com>
2015-01-26 11:36:31 +0100

    Merge branch 'danielegagliardi6-LocalDaniele'

--- lp-collaborative-workspace/lp-collaborative-workspace-bridge/lp-collaborative-workspace-bridge-importer/lp-collaborative-workspace-bridge-importer-adoxx/src/main/java/ch/fhnw/wiki/XWikiGenerator.java
+++ lp-collaborative-workspace/lp-collaborative-workspace-bridge/lp-collaborative-workspace-bridge-importer/lp-collaborative-workspace-bridge-importer-adoxx/src/main/java/ch/fhnw/wiki/XWikiGenerator.java
@@ -96,7 +96,7 @@ public class XWikiGenerator {
 				arrayName = BPMN.BPMNGenerator(instances, spaceName, xwikiUser, directoryBPMN, createDirectory, doc, docFactory);
 				//PANELS GENERATOR
 				//NodeList connector = instances.getElementsByTagName("CONNECTOR");
-				System.out.println(Panels.PanelsGenerator(connector, directoryBPMN, xwikiUser) );
+				System.out.println(Panels.PanelsGenerator(connector, directoryBPMN, xwikiUser, spaceName) );
 
 
 			}
@@ -109,6 +109,11 @@ public class XWikiGenerator {
 
 				System.out.println( "SENTRY MODEL:" );
 				System.out.println("\n");
+				
+			}else if(model.getAttributes().getNamedItem("modeltype").toString().equals("modeltype=\"BPMN + CMMN model\"")){
+				
+				System.out.println("BPMN + CMMN model are not currently being translated.");
+				
 			}
 			
 			
