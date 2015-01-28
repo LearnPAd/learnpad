d79cdaf1f26a2859b9daea0746cb4b7dc305ad5d
Jean SIMARD <woshilapin@gmail.com>
2015-01-28 15:23:58 +0100

    Add bpmn.link service
    Only template at the moment, no implementation: the interface is empty.

---
+++ lp-collaborative-workspace/lp-collaborative-workspace-component/lp-collaborative-workspace-component-service/lp-collaborative-workspace-component-service-bpmn/lp-collaborative-workspace-component-service-bpmn-link/lp-collaborative-workspace-component-service-bpmn-link-default/src/main/java/com/xwikisas/learnpad/cw/component/service/bpmn/link/internal/DefaultBPMNLinkManager.java
@@ -0,0 +1,13 @@
+package com.xwikisas.learnpad.cw.component.service.bpmn.link.internal;
+
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+
+import com.xwikisas.learnpad.cw.component.service.bpmn.link.BPMNLinkManager;
+
+@Component
+@Singleton
+public class DefaultBPMNLinkManager implements BPMNLinkManager {
+
+}
