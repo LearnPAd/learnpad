5be613fcb610b6c9e945d7e1fbb0792f62046f06
Tom Jorquera <tom.jorquera@linagora.com>
2015-02-06 16:21:27 +0100

    Minor js lib cleaning
    Format cleaning (no actual code change)

--- lp-simulation-environment/src/main/resources/Task.js
+++ lp-simulation-environment/src/main/resources/Task.js
@@ -3,61 +3,64 @@ function Task(address, taskid) {
     var ws;
 
     Task.prototype.join = function() {
-	var location = "ws://" + address + "/tasks/" + taskid
-	ws = new WebSocket(location);
-	ws.onopen = this._onopen;
-	ws.onmessage = this._onmessage;
-	ws.onclose = this._onclose;
-	ws.onerror = this._onerror;
-    }
+        var location = 'ws://' + address + '/tasks/' + taskid
+        ws = new WebSocket(location);
+        ws.onopen = this._onopen;
+        ws.onmessage = this._onmessage;
+        ws.onclose = this._onclose;
+        ws.onerror = this._onerror;
+    };
 
     Task.prototype.end = function() {
-	ws.onclose('');
-    }
+        ws.onclose('');
+    };
 
     Task.prototype.send = function(data) {
-	ws.send(data);
-    }
+        ws.send(data);
+    };
 
-    Task.prototype._onopen = function(){
+    Task.prototype._onopen = function() {
 
-	var tasksDiv = $('#tasks');
+        var tasksDiv = $('#tasks');
 
-	var taskDiv = document.createElement('div');
-	taskDiv.id = 'taskcontainer'+taskid;
-	taskDiv.innerHTML = '<p id="taskdata' + taskid + '"></p>\
-	<form id="taskform'+ taskid + '"  class="well"></form>\
-	<hr>';
+        var taskDiv = document.createElement('div');
+        taskDiv.id = 'taskcontainer' + taskid;
+        taskDiv.innerHTML = '<p id="taskdata' +
+            taskid + '"></p><form id="taskform' +
+            taskid + '"  class="well"></form><hr>';
 
-	tasksDiv.append(taskDiv);
-    }
+        tasksDiv.append(taskDiv);
+    };
 
     Task.prototype._onmessage = function(m) {
-	var data = JSON.parse(m.data);
+        var data = JSON.parse(m.data);
 
-	$('#taskdata' + taskid).html(data.description);
-	$('#taskform' + taskid).jsonForm({
-	    schema: data.form.schema,
-	    form: data.form.form,
-	    onSubmit: function (errors, values) {
-		if (!errors) {
-		    ws.send(JSON.stringify(values));
-		    return false;
-		}
-	    }});
-    }
+        $('#taskdata' + taskid).html(data.description);
+        $('#taskform' + taskid).jsonForm({
+            schema: data.form.schema,
+            form: data.form.form,
+            onSubmit: function(errors, values) {
+                if (!errors) {
+                    ws.send(JSON.stringify(values));
+                    return false;
+                }
+            }});
+    };
 
     Task.prototype._onclose = function(m) {
-	$('#taskform' + taskid).remove();
-	$('#taskcontainer' + taskid).addClass('disabled');
+        $('#taskform' + taskid).remove();
+        $('#taskcontainer' + taskid).addClass('disabled');
 
-	if(closeOnError){
-	    alert("The following error occurred: " + m.reason + " (" + m.code +")");
-	}
-    }
+        if (closeOnError) {
+            alert('The following error occurred: ' +
+                  m.reason +
+                  ' (' + m.code + ')'
+                 );
+        }
+    };
 
     Task.prototype._onerror = function(e) {
-	closeOnError = true;
-    }
+        closeOnError = true;
+    };
 }
 
--- lp-simulation-environment/src/main/resources/TaskReceiver.js
+++ lp-simulation-environment/src/main/resources/TaskReceiver.js
@@ -5,52 +5,56 @@ function TasksReceiver(address) {
 
     TasksReceiver.prototype.init = function(uiid) {
 
-	var location = "ws://" + address + "/ui/" + uiid;
-	taskReceiver._ws = new WebSocket(location);
-	taskReceiver._ws.onopen = this._onopen;
-	taskReceiver._ws.onmessage = this._onmessage;
-	taskReceiver._ws.onclose = this._onclose;
-	taskReceiver._ws.onerror = this._onerror;
-    }
+        var location = 'ws://' + address + '/ui/' + uiid;
+        taskReceiver._ws = new WebSocket(location);
+        taskReceiver._ws.onopen = this._onopen;
+        taskReceiver._ws.onmessage = this._onmessage;
+        taskReceiver._ws.onclose = this._onclose;
+        taskReceiver._ws.onerror = this._onerror;
+    };
 
     TasksReceiver.prototype.finish = function() {
-	$('#finished').removeClass('hidden');
-    }
+        $('#finished').removeClass('hidden');
+    };
 
     TasksReceiver.prototype._onopen = function() {
-	$('#connect').remove();
-    }
+        $('#connect').remove();
+    };
 
     TasksReceiver.prototype._onmessage = function(m) {
-	var msg = JSON.parse(m.data);
-	switch(msg.type) {
+        var msg = JSON.parse(m.data);
+        switch (msg.type) {
 
-	    case "FINISHED":
-		taskReceiver.finish();
-		break;
+        case 'FINISHED':
+            taskReceiver.finish();
+            break;
 
-	    case "ADDTASK":
-		var newTask = new Task(address, msg.taskid);
-		activeTasks[msg.taskid] = newTask;
-		newTask.join();
-		break;
+        case 'ADDTASK':
+            var newTask = new Task(address, msg.taskid);
+            activeTasks[msg.taskid] = newTask;
+            newTask.join();
+            break;
 
-	    case "DELTASK":
-		// may be undefined if task was closed from another ui
-		activeTasks[msg.taskid].end();
-                delete activeTasks[msg.taskid];
-		break;
-	}
-    }
+        case 'DELTASK':
+            // may be undefined if task was closed from another ui
+            activeTasks[msg.taskid].end();
+            delete activeTasks[msg.taskid];
+            break;
+        }
+    };
 
     TasksReceiver.prototype._onclose = function(m) {
-	delete taskReceiver._ws;
-	if(finishOnError){
-	    alert("The following error occurred: " + m.reason + " (" + m.code +")");
-	}
-    }
+        delete taskReceiver._ws;
+        if (finishOnError) {
+            alert(
+                'The following error occurred: ' +
+                    m.reason +
+                    ' (' + m.code + ')'
+            );
+        }
+    };
 
     TasksReceiver.prototype._onerror = function(e) {
-	finishOnError = true;
-    }
+        finishOnError = true;
+    };
 }
