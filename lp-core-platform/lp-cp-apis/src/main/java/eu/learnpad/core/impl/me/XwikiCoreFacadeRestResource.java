e60cdafef3614becce73d3d32f34f110e1da1ca2
Gulyx <guglielmo.deangelis@isti.cnr.it>
2015-05-20 15:08:17 +0200

    This is a new proposal for the bridges. This branch depends on the acceptance of the branch "another-proposal-about-controllers"
    Thus, like that other branch, it has not been discussed
    yet with Jean. Also it could be appropriate to move the
    XWIKI dependent implementation of the API (eu.learnpad.core.impl)
    in another project.
    
    Currently there was added a dependency with
    eu.learnpad.rest.utils.RestResource which is still in
    the old project implementing an XWIKI solution for
    the controllers.

---
+++ lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/core/impl/me/XwikiCoreFacadeRestResource.java
@@ -0,0 +1,46 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package eu.learnpad.core.impl.me;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.rest.XWikiRestComponent;
+
+import eu.learnpad.exception.impl.LpRestExceptionImpl;
+import eu.learnpad.me.CoreFacade;
+import eu.learnpad.rest.utils.RestResource;
+
+@Component
+public class XwikiCoreFacadeRestResource extends RestResource implements XWikiRestComponent, CoreFacade{
+
+	@Override
+	public byte[] getFeedbacks(String modelId, String type, byte[] adoxxFile)
+			throws LpRestExceptionImpl {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void putModel(String modelId, String type) throws LpRestExceptionImpl {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
