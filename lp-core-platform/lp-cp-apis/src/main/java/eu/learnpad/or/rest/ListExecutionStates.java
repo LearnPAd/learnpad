c5b119d4d5a4d3263a202f6799dc5b639c277ecb
Jean SIMARD <woshilapin@tuziwo.info>
2015-06-23 11:56:40 +0200

    API for Ontology Recommendation

---
+++ lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/or/rest/ListExecutionStates.java
@@ -0,0 +1,63 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package eu.learnpad.or.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import eu.learnpad.exception.LpRestException;
+
+// <host>/learnpad/or/execution/{userId}
+@Path("/learnpad/or/execution/{userId}")
+public interface ListExecutionStates {
+
+	/**
+	 * Returns list of execution states belonging to the given user. This list
+	 * is resolved via the users role, the assigned tasks and the latest states
+	 * on all open paths (threads) belonging to the tasks.
+	 *
+	 * The returned list of states contain the properties given when the states
+	 * were stored.
+	 *
+	 * Ex.
+	 *
+	 * <code>
+	 * <states>
+	 *     <state>
+	 *         <modelSetId>123</modelSetId>
+	 *         <executionId>343423</executionId>
+	 *         <userId>2132321</userId>
+	 *         <threadId>1.2</threadId>
+	 *         <pageId>2132321</pageId>
+	 *         <artifactId>2132321</artifactId>
+	 *     </state>
+	 * </states>
+	 * </code>
+	 *
+	 * @param userId
+	 *            the users unique id
+	 * @return list of states resp. tasks user is in charge of
+	 * @throws LpRestException
+	 */
+	@GET
+	byte[] listExecutionStates(@PathParam("userId") String userId)
+			throws LpRestException;
+}
---
+++ lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/or/rest/ResourceNotification.java
@@ -0,0 +1,45 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package eu.learnpad.or.rest;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+
+import eu.learnpad.exception.LpRestException;
+
+// <host>/learnpad/or/resourcenotification/{modelsetid}?resourceid=id,linkedto=(id1,id2,id3),action={added|modified|deleted}
+@Path("/learnpad/or/resourcenotification/{modelsetid}")
+public interface ResourceNotification {
+	/**
+	 * @param modelSetId is the ID of the model set that is concerned
+	 * @param resourceId is the ID that designate the resource
+	 * @param artifactIds is a list of ID to other artifacts from the model, linked to this resource
+	 * @param action will precise the kind of notification (added, deleted, modified)
+	 * @throws LpRestException
+	 */
+	@POST
+	void sendResourceNotification(@PathParam("modelsetid") String modelSetId,
+			@QueryParam("resourceid") String resourceId,
+			@QueryParam("linkedto") String artifactIds,
+			@QueryParam("action") String action )
+			throws LpRestException;
+}
