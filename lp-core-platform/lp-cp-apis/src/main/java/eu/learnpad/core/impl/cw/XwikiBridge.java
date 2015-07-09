f67654adc301c3502db970f5a1ecfc22c71cb6a8
Jean SIMARD <woshilapin@tuziwo.info>
2015-07-09 10:20:50 +0200

    Add XWikiBridge as a Xwiki component in CW

--- lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/core/impl/cw/XwikiBridge.java
+++ lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/core/impl/cw/XwikiBridge.java
@@ -19,9 +19,29 @@
  */
 package eu.learnpad.core.impl.cw;
 
-import eu.learnpad.cw.Bridge;
+import java.io.File;
+import java.io.InputStream;
+import java.util.UUID;
 
-public abstract class XwikiBridge extends Bridge{
+import javax.inject.Named;
+import javax.ws.rs.Path;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.xwiki.component.annotation.Component;
+
+import eu.learnpad.cw.Bridge;
+import eu.learnpad.exception.LpRestException;
+
+@Component
+@Named("eu.learnpad.core.impl.cw.XwikiBridge")
+@Path("/learnpad/cw")
+public class XwikiBridge extends Bridge{
 
 	public XwikiBridge (){
 		this(false);
@@ -32,6 +52,65 @@ public abstract class XwikiBridge extends Bridge{
 			this.corefacade = new XwikiCoreFacade();
 		else
 			this.corefacade = new XwikiCoreFacadeRestResource();
+	}
+
+	@Override
+	public byte[] getComments(String modelSetId, String artifactId)
+			throws LpRestException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] getResource(String modelSetId, String resourceId,
+			String linkedTo, String action) throws LpRestException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private String buildXWikiPackage(InputStream modelStream, String type) {
+		UUID uuid = UUID.randomUUID();
+		String stylesheetFileName = "/stylesheet/" + type + "2xwiki.xsl";
+		InputStream stylesheetStream = XwikiBridge.class
+				.getResourceAsStream(stylesheetFileName);
+		File packageFolder = new File("/tmp/learnpad/" + uuid);
+		packageFolder.mkdirs();
+
+		Source modelSource = new StreamSource(modelStream);
+		Source stylesheetSource = new StreamSource(stylesheetStream);
+		File rdfFile = new File(packageFolder.getPath() + "/ontology.rdf");
+		Result result = new StreamResult(rdfFile);
+
+		// create an instance of TransformerFactory
+		TransformerFactory transFact = new net.sf.saxon.TransformerFactoryImpl();
+
+		try {
+			Transformer trans = transFact.newTransformer(stylesheetSource);
+			trans.setParameter("packageFolder", packageFolder.getPath());
+			trans.transform(modelSource, result);
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return packageFolder.getPath() + "/xwiki";
+	}
+
+	@Override
+	public void modelSetImported(String modelSetId, String type) throws LpRestException {
+		this.corefacade.getModel(modelSetId, type);		
+	}
+
+	@Override
+	public void contentVerified(String modelSetId, String artifactId,
+			String resourceId, String result) throws LpRestException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modelVerified(String modelSetId, String result)
+			throws LpRestException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
