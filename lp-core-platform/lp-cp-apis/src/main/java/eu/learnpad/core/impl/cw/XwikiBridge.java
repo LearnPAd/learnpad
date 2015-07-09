40883973c8e1442e58870c4b31446818373f430d
Jean SIMARD <woshilapin@tuziwo.info>
2015-07-09 16:04:44 +0200

    Add modelSetId in the XSL file

--- lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/core/impl/cw/XwikiBridge.java
+++ lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/core/impl/cw/XwikiBridge.java
@@ -84,7 +84,7 @@ public class XwikiBridge extends Bridge implements XWikiRestComponent {
 		return null;
 	}
 
-	private String buildXWikiPackage(InputStream modelStream, String type) {
+	private String buildXWikiPackage(String modelSetId, InputStream modelStream, String type) {
 		UUID uuid = UUID.randomUUID();
 		String stylesheetFileName = "/stylesheet/" + type + "2xwiki.xsl";
 		InputStream stylesheetStream = XwikiBridge.class
@@ -103,6 +103,7 @@ public class XwikiBridge extends Bridge implements XWikiRestComponent {
 		try {
 			Transformer trans = transFact.newTransformer(stylesheetSource);
 			trans.setParameter("packageFolder", packageFolder.getPath());
+			trans.setParameter("modelSetId", modelSetId);
 			trans.transform(modelSource, result);
 		} catch (TransformerException e) {
 			// TODO Auto-generated catch block
@@ -119,7 +120,7 @@ public class XwikiBridge extends Bridge implements XWikiRestComponent {
 				this.cwController.getModel(modelSetId, type));
 		
 		// Make the XSL transformation and get the package's path
-		String packagePath = buildXWikiPackage(modelStream, type);
+		String packagePath = buildXWikiPackage(modelSetId, modelStream, type);
 		
 		// Now send the package's path to the importer for XWiki
 		HttpClient httpClient = RestResource.getClient();
