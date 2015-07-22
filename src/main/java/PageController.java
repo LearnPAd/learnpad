45a5507a09dad9974f80bd3950d8aa8f19a863c8
Giorgio <isiu@MacBook-Pro.local>
2015-07-22 09:24:44 +0200

    primo commit

---
+++ src/main/java/PageController.java
@@ -0,0 +1,33 @@
+
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean(name="pageController")
+@SessionScoped
+public class PageController implements Serializable {
+	public String navigatePage1() {
+		System.out.println("Redirect to Page 1");
+		
+		return "page1";
+	}
+	
+	public String navigatePage2() {
+		System.out.println("Redirect to Page 2");
+		
+		return "page2";
+	}
+	
+	public String action() {
+		System.out.println("Action Fired");
+		
+		return null;
+	}
+	
+	public void actionListener(ActionEvent event) {
+		System.out.println("ActionListener Fired");
+	}
+}
