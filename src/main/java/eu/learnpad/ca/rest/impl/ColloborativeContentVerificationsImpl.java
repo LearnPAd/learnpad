ab70192109dec429557486d6ff5740957d22b5d9
Giorgio <imatesiu@gmail.com>
2015-07-25 01:13:59 +0200

    bugfix language

--- src/main/java/eu/learnpad/ca/rest/impl/ColloborativeContentVerificationsImpl.java
+++ src/main/java/eu/learnpad/ca/rest/impl/ColloborativeContentVerificationsImpl.java
@@ -61,10 +61,10 @@ public class ColloborativeContentVerificationsImpl implements ColloborativeConte
 		if(contentFile!=null){
 			id++;
 			Language lang = null;
-			if(contentFile.getLanguage().toLowerCase()=="english"){
+			if(contentFile.getLanguage().toLowerCase().equals("english")){
 				lang = new BritishEnglish();
 			}else{
-				if(contentFile.getLanguage().toLowerCase()=="italian"){
+				if(contentFile.getLanguage().toLowerCase().equals("italian")){
 					lang = new Italian();
 				}else
 					lang = new BritishEnglish();
