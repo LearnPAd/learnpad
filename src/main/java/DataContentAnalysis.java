88d3f3291641ed2df84d5e8ab73716d34337cd98
Giorgio <imatesiu@gmail.com>
2015-07-25 17:26:51 +0200

    add color

--- src/main/java/DataContentAnalysis.java
+++ src/main/java/DataContentAnalysis.java
@@ -15,31 +15,37 @@ import eu.learnpad.ca.rest.data.collaborative.AnnotatedCollaborativeContentAnaly
 @ManagedBean(name="DataContentAnalysisbean")
 @SessionScoped
 public class DataContentAnalysis implements Serializable{
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6790649148160108414L;
 	private AnnotatedCollaborativeContentAnalysis acca;
 	private List<DataContent> listdata;
+	private String color;
 	private String element = "ele";
-	
+
 
 	public DataContentAnalysis(){
+		
 		System.out.println("DataContentAnalysisbean");
 		listdata = new ArrayList<DataContent>();
 	}
-	
+
 	/*public DataContentAnalysis(AnnotatedCollaborativeContentAnalysis acca) {
 		this.acca = acca;
 		listdata = new ArrayList<DataContent>();
 		createData();
 	}*/
+
 	
-	
-	
+
 	public String getElement() {
 		return element;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
 	}
 
 	public AnnotatedCollaborativeContentAnalysis getAcca() {
@@ -57,12 +63,39 @@ public class DataContentAnalysis implements Serializable{
 		this.element = element;
 	}
 
-	
-	
+	public String getColor(){
+		String color;
+		switch (acca.getOverallQuality()) {
+		case "VERY BAD":
+			color ="#FF0000";
+			break;
+
+		case "BAD":
+			color ="#FFFF00";
+			break;
+
+		case "GOOD":
+			color ="#00FF00";
+			break;
+		case "VERY GOOD":
+			color ="#00FF00";
+			break;
+		case "EXCELLENT":
+			color ="#00FF7F";
+			break;
+
+
+		default:
+			color ="";
+			break;
+		}
+		return color;
+	}
+
 	private void createData(){
 		Content c = acca.getCollaborativeContent().getContent();
 		List<Annotation> lannot =  acca.getAnnotations();
-		
+
 		int inode=0;
 		Node temp= null;
 		String tempString = null;
@@ -84,14 +117,14 @@ public class DataContentAnalysis implements Serializable{
 				}else{
 					inode++;
 				}
-				
-				
+
+
 			}
 		}
-		
-		
+
+
 	}
-	
+
 	private DataContent search(Node startn, Node end, List<Annotation> lannot, String element){
 		for (Annotation annotation : lannot) {
 			if(annotation.getStartNode().equals(startn.getId()) && annotation.getEndNode().equals(end.getId()) ){
@@ -110,13 +143,13 @@ public class DataContentAnalysis implements Serializable{
 	}
 
 	public String executeListener(){
-		
+
 		return "";
 	}
-	
+
 	public void listener(ActionEvent event){
-		
+		this.setAcca((AnnotatedCollaborativeContentAnalysis) event.getComponent().getAttributes().get("valdata"));
 		System.out.println(event);
 	}
-	
+
 }
