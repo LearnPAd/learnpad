bc59f10528601f64865858ad6f9de5e215935729
Giorgio <imatesiu@gmail.com>
2015-08-07 15:22:14 +0200

    welcome page upgraded

--- src/main/java/eu/learnpad/ca/main/welcomPage.java
+++ src/main/java/eu/learnpad/ca/main/welcomPage.java
@@ -2,12 +2,9 @@ package eu.learnpad.ca.main;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-import eu.learnpad.exception.LpRestException;
 
 
 
@@ -18,12 +15,20 @@ public class welcomPage {
 	
 	
 	
-	@Produces(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_HTML)
 	@GET
-	public Response getWelcom(){
-		String output =  "Welcome on Content Analysis Component! \n please use ReST Interfaces\n";
+	public String getWelcom(){
 		
-		return Response.status(200).entity(output).build();
+		
+		String welkome =  "Welcome on Content Analysis Component!";
+		
+		String stringa = "This is the Content Analysis component of the LearnPAd platform.\n please use ReST Interfaces\n";
+		
+		String page = "<html> " + "<title>" + "Content Analysis Component Page" + "</title>"
+		          + "<body style=\"white-space: pre-wrap;\"><h1>" + welkome + "</h1>"+stringa
+				+"</body>" + "</html> ";
+		
+		return page;
 	}
 	
 	
