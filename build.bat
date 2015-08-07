1782ccd3a39bc8e0e49196872e34563a4e6ff90b
Giorgio <imatesiu@gmail.com>
2015-08-07 15:05:39 +0200

    build.bat upgraded

--- build.bat
+++ build.bat
@@ -20,8 +20,19 @@ echo %__CATALINA__BASE__%
 
 
 IF DEFINED CATALINA_BASE (
+if not "tasklist /v | find /I "tomcat"" == "" (
+echo CATALINA STOP
+set originalcd=%cd%
+cd %CATALINA_BASE%\bin\
+call shutdown.bat
+cd %originalcd%
+call PING -n 3 127.0.0.1>nul
+)
 if exist %__CATALINA__WEB__%\contentanalysis.war (	del /S /F /A %__CATALINA__WEB__%\contentanalysis.war)
-if exist %__CATALINA__WEB__%\contentanalysis	    ( rmdir  %__CATALINA__WEB__%\contentanalysis /s /q)
+if exist %__CATALINA__WEB__%\contentanalysis	    ( 
+
+rmdir  %__CATALINA__WEB__%\contentanalysis /s /q
+)
 call mvn clean
 if exist %cd%\target		(rmdir target /s /q)
 ) ELSE (
@@ -32,4 +43,7 @@ if exist %cd%\target		(rmdir target /s /q)
 @REM skip tests as we will do them in the component_test stage
 call mvn  package -Dmaven.test.skip=true
 xcopy /y target\contentanalysis.war %__CATALINA__WEB__%
-
+set originalcd=%cd%
+cd %CATALINA_BASE%\bin\
+call startup.bat
+cd %originalcd%
