555d31068988cb1d20e7a8cc5774c98ff355fb7e
damianofalcioni <damiano.falcioni@unicam.it>
2015-08-17 17:50:56 +0200

    plugin interface description update

--- lp-verification-component/src/main/java/eu/learnpad/verification/plugin/interfaces/Plugin.java
+++ lp-verification-component/src/main/java/eu/learnpad/verification/plugin/interfaces/Plugin.java
@@ -1,6 +1,19 @@
 package eu.learnpad.verification.plugin.interfaces;
-
+/*
+ * This interface should be implemented in order to create a valid plugin for the verification component
+ */
 public interface Plugin {
+	/**
+	 * The method return a list of verification capabilities provided by the plugin
+	 * @return	an array of string identifying the verification type provided
+	 */
 	public String[] getVerificationTypeProvided();
-	public String performVerification(String model, String Type); //FIXME: valutare come passare il modello e in che formato ritornare il risultato.
+	
+	/**
+	 * The method perform the verification specified over the provided model. The verification have to be done in a synchronous way. Asynchronous support is provided by the verification component that manage the plugins.
+	 * @param	model	The model to verify (currently the model passed is the full ADOxx LearnPAd xml)
+	 * @param	type	The type of verification to perform; one provided by the method getVerificationTypeProvided
+	 * @return 	The verification result in XML format. The root element have to be <Result><Result/>
+	 */
+	public String performVerification(String model, String type);
 }
