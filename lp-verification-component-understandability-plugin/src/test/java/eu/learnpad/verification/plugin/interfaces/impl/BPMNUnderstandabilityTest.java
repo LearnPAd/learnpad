621c23e0ce2fbfb8dfec2d6376956711230a0273
Giorgio <isiu@MacBook-Pro.local>
2015-09-05 12:16:06 +0200

    test plugin added

---
+++ lp-verification-component-understandability-plugin/src/test/java/eu/learnpad/verification/plugin/interfaces/impl/BPMNUnderstandabilityTest.java
@@ -0,0 +1,25 @@
+package eu.learnpad.verification.plugin.interfaces.impl;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+import org.junit.Test;
+
+import eu.learnpad.verification.plugin.bpmn.guideline.factory.GuidelinesFactoryTest;
+
+public class BPMNUnderstandabilityTest {
+
+	@Test
+	public void test() {
+		InputStream is = GuidelinesFactoryTest.class.getClassLoader().getResourceAsStream("annidategateway.bpmn");
+		assertNotNull(is);
+		BPMNUnderstandability understandability = new BPMNUnderstandability();
+		String[]  type = understandability.getVerificationTypeProvided();
+		String model= new Scanner(is).useDelimiter("\\A").next();
+		String result = understandability.performVerification(model, type[0]);
+		assertNotNull(result);
+	}
+
+}
