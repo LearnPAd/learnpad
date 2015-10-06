631f4d23bdb9e66b5c401148647ac24a58b4335a
Giorgio <imatesiu@gmail.com>
2015-10-06 17:54:48 +0200

    class abstratc to analysis created

---
+++ lp-content-analysis/src/main/java/eu/learnpad/ca/analysis/AbstractAnalysisClass.java
@@ -0,0 +1,108 @@
+package eu.learnpad.ca.analysis;
+
+import java.util.Map;
+
+import org.languagetool.Language;
+
+import eu.learnpad.ca.rest.data.collaborative.AnnotatedCollaborativeContentAnalysis;
+import eu.learnpad.ca.rest.data.collaborative.CollaborativeContentAnalysis;
+import eu.learnpad.ca.rest.data.stat.AnnotatedStaticContentAnalysis;
+import eu.learnpad.ca.rest.data.stat.StaticContentAnalysis;
+
+public abstract class AbstractAnalysisClass extends Thread{
+
+	
+	protected AnnotatedCollaborativeContentAnalysis annotatedCollaborativeContent;
+	protected AnnotatedStaticContentAnalysis annotatedStaticContent;
+	
+	protected Language language;
+	protected Integer numDefectiveSentences = 0;
+	protected CollaborativeContentAnalysis collaborativeContentInput;
+	protected StaticContentAnalysis staticContentInput;
+
+
+	public String getStatus(){
+		switch (this.getState()) {
+		case TERMINATED:
+			return "OK";
+
+		default:
+			return "IN PROGRESS";
+		}
+
+	}
+
+	protected double calculateOverallQualityMeasure(Integer numsentence){
+		double qm = (1-(numDefectiveSentences.doubleValue()/numsentence.doubleValue()))*100;
+		double qualityMeasure = Math.abs(qm);
+		return qualityMeasure;
+	}
+
+	protected String calculateOverallQuality(double qualityMeasure){
+		String quality="";
+		if(qualityMeasure<=25){
+			quality="VERY BAD";
+		}else if(qualityMeasure<=50){
+			quality="BAD";
+		}else if(qualityMeasure<=75){
+			quality="GOOD";
+		}else if(qualityMeasure<100){
+			quality="VERY GOOD";
+		}else if(qualityMeasure==100){
+			quality="EXCELLENT";
+		}
+		return quality;
+	}
+
+	protected String calculateOverallRecommendations(double qualityMeasure){
+		String recommendations="";
+		if(qualityMeasure<=25){
+			recommendations="Quality is very poor, correct the errors";
+		}else if(qualityMeasure<=50){
+			recommendations="Quality is poor, correct the errors";
+		}else if(qualityMeasure<=75){
+			recommendations="Quality is acceptable, but there are still some errors";
+		}else if(qualityMeasure<100){
+			recommendations="Well done, still few errors remaining";
+		}else if(qualityMeasure==100){
+			recommendations="Well done, no errors found!";
+		}
+		return recommendations;
+	}
+
+
+	public AnnotatedCollaborativeContentAnalysis getAnnotatedCollaborativeContentAnalysis(){
+		return annotatedCollaborativeContent;
+	}
+
+	public AnnotatedStaticContentAnalysis getAnnotatedStaticContentAnalysis() {
+		return annotatedStaticContent;
+	} 
+	
+	protected int indexofElement(String sentence, String word, Map<String, Integer> elementfinded){
+		String [] spliter = sentence.split("[\\W]");
+		int position = 0;
+		int numwordfinded = 0;
+		for (int i = 0; i < spliter.length; i++) {
+			int offset = 0;
+			String token = spliter[i];
+			if(token.equals(word)){
+				numwordfinded++;
+				if(!elementfinded.containsKey(token)){
+					elementfinded.put(token, 1);
+				}
+				if(elementfinded.get(token).intValue()==numwordfinded){
+					Integer I = elementfinded.get(token);
+					int y  = I.intValue()+1;
+					elementfinded.put(token, y);
+					return position;
+				}else{
+					position+=token.length()+1+offset;
+				}
+			}else{
+				position+=token.length()+1+offset;
+			}
+		}
+		return position;
+	}
+}
