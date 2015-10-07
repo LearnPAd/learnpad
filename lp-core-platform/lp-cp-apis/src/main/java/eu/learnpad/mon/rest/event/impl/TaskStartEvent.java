052ecaacc13ad2d8532207dc1d0619bd563d9741
Tom Jorquera <tom.jorquera@linagora.com>
2015-10-07 10:31:22 +0200

    Add REST API for receiving monitoring events
    Add a new "mon" API for receiving events sent by the simulator
    monitoring component.
    
    Currently, 5 event types are defined: processes start and stop, taskes
    start and stop, and session score updates.
    
    For each event type, the REST API exposes a dedicated POST method, along
    with an AbstractEvent implementation.

---
+++ lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/mon/rest/event/impl/TaskStartEvent.java
@@ -0,0 +1,72 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package eu.learnpad.mon.rest.event.impl;
+
+import java.util.List;
+
+import eu.learnpad.mon.rest.event.AbstractEvent;
+import eu.learnpad.mon.rest.event.EventType;
+
+/**
+ *
+ * @author Tom Jorquera - Linagora
+ *
+ */
+public class TaskStartEvent extends AbstractEvent {
+
+	/**
+	 * Unique ID of the process instance
+	 */
+	public String processid;
+
+	/**
+	 * Unique ID of the task instance
+	 */
+	public String taskid;
+
+	/**
+	 * ID used to identify the task in the BP definition
+	 */
+	public String taskdefid;
+
+	/**
+	 * The LearnPAd users that have been assigned to this task
+	 */
+	public List<String> assignedusers;
+
+	public TaskStartEvent() {
+		super();
+	}
+
+	public TaskStartEvent(Long timestamp, String processid, String taskid,
+			String taskdefid, List<String> assignedusers) {
+		super(timestamp);
+		this.processid = processid;
+		this.taskid = taskid;
+		this.taskdefid = taskdefid;
+		this.assignedusers = assignedusers;
+	}
+
+	@Override
+	public EventType getType() {
+		return EventType.TASK_START;
+	}
+
+}
