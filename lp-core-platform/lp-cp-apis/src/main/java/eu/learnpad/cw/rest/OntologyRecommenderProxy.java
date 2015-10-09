399bd29f64c10de564d00782429327bb1298219d
Jean SIMARD <woshilapin@tuziwo.info>
2015-10-09 14:48:40 +0200

    [cp] Fix problem with @Path in the API

--- lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/cw/rest/OntologyRecommenderProxy.java
+++ lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/cw/rest/OntologyRecommenderProxy.java
@@ -26,11 +26,11 @@ import javax.ws.rs.QueryParam;
 import eu.learnpad.exception.LpRestException;
 import eu.learnpad.or.rest.data.Recommendations;
 
-@Path("/recommendation")
 public interface OntologyRecommenderProxy {
-
 	@GET
-	public Recommendations getRecommendations(@QueryParam("modelsetid") String modelSetId,
+	@Path("/recommendation")
+	Recommendations getRecommendations(
+			@QueryParam("modelsetid") String modelSetId,
 			@QueryParam("artifactid") String artifactId,
 			@QueryParam("userid") String userId) throws LpRestException;
 }
