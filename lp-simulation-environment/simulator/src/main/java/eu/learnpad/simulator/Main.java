c67fa7ee489406a39d1a60aba6e8afc4fd5981ef
Tom Jorquera <tom.jorquera@linagora.com>
2015-10-09 10:45:35 +0200

    [sim] Change default loaded process to TitoloUnico
    For demo purpose, the Titolo Unico conference of services process is the
    one loaded by default from now.

--- lp-simulation-environment/simulator/src/main/java/eu/learnpad/simulator/Main.java
+++ lp-simulation-environment/simulator/src/main/java/eu/learnpad/simulator/Main.java
@@ -58,7 +58,7 @@ public class Main {
 
 			// load process definitions
 			simulator.processManager().addProjectDefinitions(
-					DEMO_PROCESS_FOLDER + "/demo-suap-1.bpmn20.xml");
+					DEMO_PROCESS_FOLDER + "/titolounico.bpmn20.xml");
 
 			System.out.println("---\n");
 			System.out.println("Demo is ready and can be accessed at http://"
