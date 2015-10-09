170adc65b414b0e5fbdc6ac91bf961bd3fe7a60c
Giorgio <imatesiu@gmail.com>
2015-10-09 16:06:37 +0200

    fix build bat for windows

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 
 ::set CATALINA_BASE=C:\Users\winspa\workspace_kep\tom
+::set CATALINA_HOME=C:\Users\winspa\workspace_kep\tom
 ::set PATH=%PATH%;C:\Users\winspa\Downloads\apache-maven-3.3.3-bin\apache-maven-3.3.3\bin
 
 set __CATALINA__BASE__=%CATALINA_BASE%
@@ -20,12 +21,9 @@ echo %__CATALINA__BASE__%
 
 
 IF DEFINED CATALINA_BASE (
-if not "tasklist /v | find /I "tomcat"" == "" (
+if "tasklist /v | find /I "tomcat"" == "" (
 echo CATALINA STOP
-set originalcd=%cd%
-cd %CATALINA_BASE%\bin\
-call shutdown.bat
-cd %originalcd%
+call %CATALINA_BASE%\bin\shutdown.bat
 call PING -n 3 127.0.0.1>nul
 )
 if exist %__CATALINA__WEB__%\lp-content-analysis.war (	del /S /F /A %__CATALINA__WEB__%\lp-content-analysis.war)
@@ -33,17 +31,16 @@ if exist %__CATALINA__WEB__%\lp-content-analysis	    (
 
 rmdir  %__CATALINA__WEB__%\lp-content-analysis /s /q
 )
+
 call mvn clean
-if exist %cd%\target		(rmdir target /s /q)
+if exist target		(rmdir target /s /q)
 ) ELSE (
     echo Please: set CATALINA_BASE
 )
 
 
 @REM skip tests as we will do them in the component_test stage
-call mvn  package -Dmaven.test.skip=trues
+call mvn  package -Dmaven.test.skip=true
 xcopy /y target\lp-content-analysis.war %__CATALINA__WEB__%
-set originalcd=%cd%
-cd %CATALINA_BASE%\bin\
-call startup.bat
-cd %originalcd%
+call %CATALINA_BASE%\bin\startup.bat
+
