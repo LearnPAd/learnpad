68b7f683316d30eb201977518fbeed553a06bd57
sandro.emmenegger <sandro.emmenegger@fhnw.ch>
2015-10-22 17:38:38 +0200

    changed all to resource stream

--- lp-ontology-recommender/src/main/java/eu/learnpad/ontology/persistence/FileOntAO.java
+++ lp-ontology-recommender/src/main/java/eu/learnpad/ontology/persistence/FileOntAO.java
@@ -9,7 +9,7 @@ import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import eu.learnpad.ontology.config.APP;
-import eu.learnpad.ontology.persistence.util.OntologyFileLoader;
+import eu.learnpad.ontology.persistence.util.OntologyResourceLoader;
 import eu.learnpad.ontology.transformation.SimpleModelTransformator;
 import java.io.File;
 import java.io.IOException;
@@ -25,44 +25,38 @@ import org.apache.jena.riot.Lang;
 @Singleton
 public class FileOntAO extends OntAO {
 
-	@Override
-	protected OntModel loadMetaModel() {
-		OntModel model = null;
-		try {
-			model = OntologyFileLoader.loadModel(
-					APP.CONF.getStringArray("ontology.metamodel.path"),
-					Lang.TTL);
-		} catch (IOException ex) {
-			Logger.getLogger(FileOntAO.class.getName()).log(Level.SEVERE, null,
-					ex);
-		}
-		return model;
-	}
+    @Override
+    protected OntModel loadMetaModel() {
+        OntModel model = null;
+        try {
+            model = OntologyResourceLoader.loadModel(APP.CONF.getStringArray("ontology.metamodel.path"), Lang.TTL);
+        } catch (IOException ex) {
+            Logger.getLogger(FileOntAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return model;
+    }
 
-	@Override
-	protected OntModel loadModelSet(String modelSetId) {
-		File modelSetFile = SimpleModelTransformator.getInstance().getLatestVersionFile(modelSetId);
-		if (modelSetFile == null) {
-			return null;
-		}
+    @Override
+    protected OntModel loadModelSet(String modelSetId) {
+        File modelSetFile = SimpleModelTransformator.getInstance().getLatestVersionFile(modelSetId);
+        if (modelSetFile == null) {
+            return null;
+        }
 
-		OntModel modelSet = ModelFactory
-				.createOntologyModel(OntModelSpec.OWL_MEM);
-		modelSet.read(modelSetFile.toURI().toString(), "TTL");
-		return modelSet;
-	}
+        OntModel modelSet = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
+        modelSet.read(modelSetFile.toURI().toString(), "TTL");
+        return modelSet;
+    }
 
-	@Override
-	protected OntModel loadExecutionData(String modelSetId) {
-		OntModel model = null;
-		try {
-			model = OntologyFileLoader.loadModel(
-					APP.CONF.getStringArray("execution.data.path"), Lang.TTL);
-		} catch (IOException ex) {
-			Logger.getLogger(FileOntAO.class.getName()).log(Level.SEVERE, null,
-					ex);
-		}
-		return model;
-	}
+    @Override
+    protected OntModel loadExecutionData(String modelSetId) {
+        OntModel model = null;
+        try {
+            model = OntologyResourceLoader.loadModel(APP.CONF.getStringArray("execution.data.path"), Lang.TTL);
+        } catch (IOException ex) {
+            Logger.getLogger(FileOntAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return model;
+    }
 
 }
