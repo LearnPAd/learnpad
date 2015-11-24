2cb801a6618605e24b10260551df0b4b6a0aedb9
Jean SIMARD <woshilapin@tuziwo.info>
2015-11-24 13:14:14 +0200

    [build] Attempt to fix the build system

--- build-system/utils.sh
+++ build-system/utils.sh
@@ -14,7 +14,9 @@ declare -r _ERROR_LEVEL_=1
 declare -r _INFO_LEVEL_=2
 declare -r _DEBUG_LEVEL_=3
 declare -r LOG_SCRIPT="${0}"
-declare -r LOG_ERROR_CODE=${3}
+if [$# -ge 3 ]
+	declare -r LOG_ERROR_CODE=${3}
+fi
 if [ $# -ge 2 ]
 then
 	declare -r LOG_MESSAGE="${2}"
