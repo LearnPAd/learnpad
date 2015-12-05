b27349f249cfc7dd43739ae2adef8a5d4283feb3
Giorgio <isiu@MacBook-Pro-3.local>
2015-12-05 11:36:43 +0100

    RestrictUsageTerminateEndEvent added

---
+++ lp-model-verification/verification-component-understandability-plugin/src/main/java/eu/learnpad/verification/plugin/bpmn/guideline/impl/RestrictUsageTerminateEndEvent.java
@@ -0,0 +1,109 @@
+package eu.learnpad.verification.plugin.bpmn.guideline.impl;
+
+
+
+import org.eclipse.bpmn2.Definitions;
+import org.eclipse.bpmn2.EndEvent;
+import org.eclipse.bpmn2.FlowElement;
+import org.eclipse.bpmn2.Process;
+import org.eclipse.bpmn2.RootElement;
+import org.eclipse.bpmn2.SubProcess;
+import org.eclipse.bpmn2.TerminateEventDefinition;
+
+
+
+public class RestrictUsageTerminateEndEvent extends abstractGuideline {
+	
+	public RestrictUsageTerminateEndEvent(Definitions diagram) {
+		super(diagram);
+		this.id = "15";
+		this.Description = "The process modeler should use terminate events only when strictly necessary; they are used to model situations where several alternative paths are enabled and the entire process have to be finished when one of them is completed. The modeler should use other end events rather than the terminate end event (e.g. a generic end event), to guarantee that the executions of the reaming process paths or activities will not be stopped.";
+		this.Name = "Restrict usage of terminate end event";
+
+
+	}
+
+	@Override
+	protected void findGL(Definitions diagram) {
+		StringBuilder temp = new StringBuilder();
+		/*Collection<FlowElement> elementsBPMNtemp = new ArrayList<FlowElement>();
+		Collection<ElementID> Elementstemp = new ArrayList<ElementID>();*/
+		int num = 0;
+	
+		
+		for (RootElement rootElement : diagram.getRootElements()) {
+			if (rootElement instanceof Process) {
+				Process process = (Process) rootElement;
+				//System.out.format("Found a process: %s\n", process.getName());
+				
+				IDProcess = process.getId();
+				for (FlowElement fe : process.getFlowElements()) {
+					if(fe instanceof SubProcess){
+						SubProcess sub = (SubProcess) fe;
+						//System.out.format("Found a SubProcess: %s\n", sub.getName());
+						this.searchSubProcess(sub);
+					}else
+						if (fe instanceof TerminateEventDefinition) {
+							num++;
+								elementsBPMN.add(fe);
+								setElements(fe.getId(),IDProcess);
+								temp.append("* name=" + fe.getName() + " ID=" + fe.getId()
+										+ "\n");
+								
+								/*elementsBPMNtemp.add(fe);
+								Elementstemp.add(new ElementID(fe.getId(),IDProcess));
+								temp.append("* name=" + fe.getName() + " ID=" + fe.getId()
+										+ "\n");*/
+							
+						} 
+				}
+			}
+		}
+		if (num>0) {
+			/*elementsBPMN.addAll(elementsBPMNtemp);
+			setAllElements(Elementstemp);*/
+			this.Suggestion = "\nDon't use Terminate End Event :" + temp;
+			this.status = false;
+		}else{
+			this.status = true;
+			this.Suggestion = "Well done!";
+		}
+	}
+
+	protected void searchSubProcess(SubProcess sub){
+		StringBuilder temp = new StringBuilder();
+		/*Collection<FlowElement> elementsBPMNtemp = new ArrayList<FlowElement>();
+		Collection<ElementID> Elementstemp = new ArrayList<ElementID>();*/
+		int num = 0;
+		for ( FlowElement fe : sub.getFlowElements()) {
+			if(fe instanceof SubProcess){
+				SubProcess ssub = (SubProcess) fe;
+				//System.out.format("Found a SubProcess: %s\n", ssub.getName());
+				this.searchSubProcess(ssub);
+			}else
+			if (fe instanceof EndEvent) {
+				
+				
+				//System.out.println(fe.eClass().getName() + ": name="+ fe.getName() + " ID=" + fe.getId());
+				num++;
+				
+					/*elementsBPMNtemp.add(fe);
+					Elementstemp.add(new ElementID(fe.getId(),IDProcess));
+					temp.append("* name=" + fe.getName() + " ID=" + fe.getId()
+							+ "\n");*/
+				elementsBPMN.add(fe);
+				setElements(fe.getId(),IDProcess);
+				temp.append("* name=" + fe.getName() + " ID=" + fe.getId()
+						+ "\n");
+			
+			}
+		}
+		if ( num>0) {
+		/*	elementsBPMN.addAll(elementsBPMNtemp);
+			setAllElements(Elementstemp);*/
+			this.Suggestion += "\nDon't use Terminate Event in SubProcess "+sub.getName()+" " + temp;
+			this.status = false;
+		}
+		
+	}
+}
