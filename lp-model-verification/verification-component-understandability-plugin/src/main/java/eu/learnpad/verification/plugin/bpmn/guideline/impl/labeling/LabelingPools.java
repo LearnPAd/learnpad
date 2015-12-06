f145497bebc8e068a925acbdba67cc576ee2e393
Giorgio <isiu@MacBook-Pro-3.local>
2015-12-06 15:08:38 +0100

    labeling pools added

--- lp-model-verification/verification-component-understandability-plugin/src/main/java/eu/learnpad/verification/plugin/bpmn/guideline/impl/labeling/LabelingPools.java
+++ lp-model-verification/verification-component-understandability-plugin/src/main/java/eu/learnpad/verification/plugin/bpmn/guideline/impl/labeling/LabelingPools.java
@@ -3,13 +3,13 @@ package eu.learnpad.verification.plugin.bpmn.guideline.impl.labeling;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+import org.eclipse.bpmn2.Collaboration;
 import org.eclipse.bpmn2.Definitions;
-import org.eclipse.bpmn2.EndEvent;
 import org.eclipse.bpmn2.FlowElement;
-import org.eclipse.bpmn2.Process;
+import org.eclipse.bpmn2.Participant;
 import org.eclipse.bpmn2.RootElement;
-import org.eclipse.bpmn2.SubProcess;
 
 import eu.learnpad.verification.plugin.bpmn.guideline.impl.abstractGuideline;
 import eu.learnpad.verification.plugin.utils.ElementID;
@@ -35,39 +35,37 @@ public class LabelingPools extends abstractGuideline{
 	
 		
 		for (RootElement rootElement : diagram.getRootElements()) {
-			if (rootElement instanceof Process) {
-				Process process = (Process) rootElement;
+			if (rootElement instanceof Collaboration) {
+				Collaboration collaboration = (Collaboration) rootElement;
 				//System.out.format("Found a process: %s\n", process.getName());
-				
-				IDProcess = process.getId();
-				for (FlowElement fe : process.getFlowElements()) {
-					if(fe instanceof SubProcess){
-						SubProcess sub = (SubProcess) fe;
-						//System.out.format("Found a SubProcess: %s\n", sub.getName());
-						this.searchSubProcess(sub);
-					}else
-						if (fe instanceof EndEvent) {
-							num++;
-							
-								/*elementsBPMN.add(fe);
-								setElements(fe.getId(),IDProcess);
-								ret.append(i++ +") name=" + fe.getName()!=null? fe.getName() : "Unlabeled" + " ID=" + fe.getId()
-										+ "\n");*/
-								
-								elementsBPMNtemp.add(fe);
-								Elementstemp.add(new ElementID(fe.getId(),IDProcess));
-								String name = fe.getName()!=null? fe.getName() : "Unlabeled"; 
-								temp.append("* name=" + name + " ID=" + fe.getId()
-										+ "\n");
-							
-						} 
+
+
+
+				List<Participant> partecipants = collaboration.getParticipants();
+				for (Participant participant : partecipants) {
+					participant.getId();
+					IDProcess = participant.getProcessRef().getId();
+					if(participant.getName()==null){
+						num++;
+
+						/*elementsBPMN.add(fe);
+					setElements(fe.getId(),IDProcess);
+					ret.append(i++ +") name=" + fe.getName() + " ID=" + fe.getId()
+							+ "\n");*/
+
+						//elementsBPMNtemp.add(participant);
+						String name = participant.getName()!=null? participant.getName() : "Unlabeled"; 
+						Elementstemp.add(new ElementID(participant.getId(),IDProcess));
+						temp.append("Name=" +name + " ID=" + participant.getId()
+								+ "; ");
+					}
 				}
 			}
 		}
 		if (num>1) {
 			elementsBPMN.addAll(elementsBPMNtemp);
 			setAllElements(Elementstemp);
-			this.Suggestion += "\nUse only one End End Event :" + temp;
+			this.Suggestion += "\nLabeling Pools:" + temp;
 			this.status = false;
 		}else{
 			this.status = true;
@@ -75,39 +73,6 @@ public class LabelingPools extends abstractGuideline{
 		}
 	}
 
-	protected void searchSubProcess(SubProcess sub){
-		StringBuilder temp = new StringBuilder();
-		Collection<FlowElement> elementsBPMNtemp = new ArrayList<FlowElement>();
-		Collection<ElementID> Elementstemp = new ArrayList<ElementID>();
-		int num = 0;
-		for ( FlowElement fe : sub.getFlowElements()) {
-			if(fe instanceof SubProcess){
-				SubProcess ssub = (SubProcess) fe;
-				//System.out.format("Found a SubProcess: %s\n", ssub.getName());
-				this.searchSubProcess(ssub);
-			}else
-			if (fe instanceof EndEvent) {
-				
-				
-				//System.out.println(fe.eClass().getName() + ": name="+ fe.getName()!=null? fe.getName() : "Unlabeled" + " ID=" + fe.getId());
-				num++;
-				
-					elementsBPMNtemp.add(fe);
-					Elementstemp.add(new ElementID(fe.getId(),IDProcess));
-					String name = fe.getName()!=null? fe.getName() : "Unlabeled"; 
-					temp.append("* name=" + name + " ID=" + fe.getId()
-							+ "\n");
-				
-			
-			}
-		}
-		if ( num>1) {
-			elementsBPMN.addAll(elementsBPMNtemp);
-			setAllElements(Elementstemp);
-			this.Suggestion += "\nUse only one End Event in SubProcess "+sub.getName()+" " + temp;
-			this.status = false;
-		}
-		
-	}
+
 
 }
