f77ea438ff5805cdeb9b2d688066a9beed755229
Giorgio <isiu@MacBook-Pro-3.local>
2015-12-08 11:27:51 +0100

    LinearSequenceFlows added

---
+++ lp-model-verification/verification-component-understandability-plugin/src/main/java/eu/learnpad/verification/plugin/bpmn/guideline/impl/appearence/LinearSequenceFlows.java
@@ -0,0 +1,99 @@
+package eu.learnpad.verification.plugin.bpmn.guideline.impl.appearence;
+
+
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.bpmn2.BaseElement;
+import org.eclipse.bpmn2.Definitions;
+import org.eclipse.bpmn2.FlowElement;
+import org.eclipse.bpmn2.Process;
+import org.eclipse.bpmn2.RootElement;
+import org.eclipse.bpmn2.SequenceFlow;
+import org.eclipse.bpmn2.di.BPMNEdge;
+import org.eclipse.dd.dc.Point;
+
+import eu.learnpad.verification.plugin.bpmn.guideline.impl.abstractGuideline;
+import eu.learnpad.verification.plugin.bpmn.reader.BPMNUtils;
+
+public class LinearSequenceFlows extends abstractGuideline{
+
+
+	public LinearSequenceFlows(Definitions diagram) {
+		super(diagram);
+		this.id = "45";
+		this.Description = "Linear sequence flows without useless foldings help to maintain the model clear.";
+		this.Name = "Linear sequence flows";
+
+
+	}
+
+	@Override
+	protected void findGL(Definitions diagram) {
+		/*StringBuilder temp = new StringBuilder();
+		Collection<FlowElement> elementsBPMNtemp = new ArrayList<FlowElement>();
+		Collection<ElementID> Elementstemp = new ArrayList<ElementID>();
+*/
+
+		int num = 0;
+		Map<BaseElement,BPMNEdge> BPMNEdges = BPMNUtils.getAllBPMNEdge(diagram);
+		for (RootElement rootElement : diagram.getRootElements()) {
+			if (rootElement instanceof Process) {
+				Process process = (Process) rootElement;
+				// System.out.format("Found a process: %s\n",
+				// process.getName());
+
+				IDProcess = process.getId();
+				for (FlowElement fe : process.getFlowElements()) {
+					if(fe instanceof SequenceFlow){
+						SequenceFlow sf = (SequenceFlow) fe;
+						//BPMNEdge res = BPMNUtils.findBPMNEdge(diagram, sf);
+						BaseElement base= (BaseElement)sf;
+						boolean resd = BPMNEdges.containsKey(base);
+						if(resd){
+							List<Point> points = BPMNEdges.get(base).getWaypoint();
+							if(points!=null)
+								if(points.size()>2){
+									num++;
+
+									elementsBPMN.add(fe);
+									String name = sf.getName()!=null? sf.getName() : "Unlabeled"; 
+									setElements(fe.getId(),IDProcess,name);
+									//ret.append(i++ +") name=" + name + " ID=" + fe.getId()
+									//		+ "\n");
+								}
+						}
+					}
+
+				}
+			}
+		}
+
+
+	//
+		/*	for (BPMNEdge bpmnEdge : BPMNEdges.values()) {
+			BaseElement bpmnelement = bpmnEdge.getBpmnElement();
+			if(bpmnelement instanceof SequenceFlow){
+				bpmnelement.getId();
+				List<Point> points = bpmnEdge.getWaypoint();
+				if(points!=null)
+					if(points.size()>2){
+
+					}
+			}
+		}
+
+		*/
+		if (num>1) {
+			
+			this.Suggestion += "\nUse Linear sequence flows: ";
+			this.status = false;
+		}else{
+			this.status = true;
+			this.Suggestion += "Well done!";
+		}
+	}
+
+
+
+}
--- lp-model-verification/verification-component-understandability-plugin/src/main/java/eu/learnpad/verification/plugin/bpmn/reader/BPMNUtils.java
+++ lp-model-verification/verification-component-understandability-plugin/src/main/java/eu/learnpad/verification/plugin/bpmn/reader/BPMNUtils.java
@@ -1,15 +1,19 @@
 package eu.learnpad.verification.plugin.bpmn.reader;
 
+import java.util.HashMap;
+
 import org.eclipse.bpmn2.BaseElement;
 import org.eclipse.bpmn2.Definitions;
 import org.eclipse.bpmn2.di.BPMNDiagram;
+import org.eclipse.bpmn2.di.BPMNEdge;
 import org.eclipse.bpmn2.di.BPMNShape;
 import org.eclipse.dd.di.DiagramElement;
 
+
 public class BPMNUtils {
-	
+
 	public static BPMNShape findBPMNShape(Definitions definitions, BaseElement baseElement) {
-		
+
 		if (definitions!=null) {
 			for (BPMNDiagram d : definitions.getDiagrams()) {
 				BPMNShape bpmnShape = findBPMNShape(d, baseElement);
@@ -19,7 +23,7 @@ public class BPMNUtils {
 		}
 		return null;
 	}
-	
+
 
 	public static BPMNShape findBPMNShape(BPMNDiagram bpmnDiagram, BaseElement baseElement) {
 		for (DiagramElement de : bpmnDiagram.getPlane().getPlaneElement()) {
@@ -32,6 +36,52 @@ public class BPMNUtils {
 		return null;
 	}
 
+	public static BPMNEdge findBPMNEdge(Definitions definitions, BaseElement baseElement) {
 
-	
+		if (definitions!=null) {
+			for (BPMNDiagram d : definitions.getDiagrams()) {
+				BPMNEdge bpmnEdge = findBPMNEdge(d, baseElement);
+				if (bpmnEdge!=null)
+					return bpmnEdge;
+			}
+		}
+		return null;
+	}
+
+
+
+	public static  BPMNEdge findBPMNEdge(BPMNDiagram bpmnDiagram, BaseElement baseElement) {
+		for (DiagramElement de : bpmnDiagram.getPlane().getPlaneElement()) {
+			if (de instanceof BPMNEdge) {
+				BaseElement ele = ((BPMNEdge)de).getBpmnElement();
+				if (ele == baseElement)
+					return (BPMNEdge)de;
+			}
+		}
+		return null;
+	}
+
+	public static java.util.Map<BaseElement,BPMNEdge>  getAllBPMNEdge(Definitions definitions) {
+
+		if (definitions!=null) {
+			for (BPMNDiagram d : definitions.getDiagrams()) {
+				java.util.Map<BaseElement,BPMNEdge>  bpmnEdges = getAllBPMNEdge(d);
+				if (bpmnEdges!=null)
+					return bpmnEdges;
+			}
+		}
+		return null;
+	}
+
+	public static  java.util.Map<BaseElement,BPMNEdge>  getAllBPMNEdge(BPMNDiagram bpmnDiagram) {
+		java.util.Map<BaseElement,BPMNEdge>  colledge = new HashMap<BaseElement, BPMNEdge>();
+		for (DiagramElement de : bpmnDiagram.getPlane().getPlaneElement()) {
+			if (de instanceof BPMNEdge) {
+				BPMNEdge edge = (BPMNEdge)de;
+				colledge.put(edge.getBpmnElement(),edge);
+			}
+		}
+		return colledge;
+	}
+
 }
