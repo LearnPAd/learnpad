3e5608c0df93ba693bf1c0e0b9ea724366ff80f2
Giorgio <imatesiu@gmail.com>
2015-12-17 17:27:24 +0100

    add interface for list of analysis

--- src/main/java/eu/learnpad/ca/analysis/AbstractAnalysisClass.java
+++ src/main/java/eu/learnpad/ca/analysis/AbstractAnalysisClass.java
@@ -3,7 +3,6 @@ package eu.learnpad.ca.analysis;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import org.languagetool.Language;
 
--- src/main/java/eu/learnpad/ca/rest/impl/ColloborativeContentVerificationsImpl.java
+++ src/main/java/eu/learnpad/ca/rest/impl/ColloborativeContentVerificationsImpl.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -13,7 +14,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.languagetool.Language;
 import org.languagetool.language.AmericanEnglish;
@@ -196,5 +199,28 @@ public class ColloborativeContentVerificationsImpl implements ColloborativeConte
 		}
 	}
 
+	@Path("/allid")
+	@GET
+	public String  getStatusCollaborativeContentVerifications()
+			throws LpRestException{
+		String result = new String();
+		try{
+			if(!map.isEmpty()){
+				for(Integer key :map.keySet()){
+					result+=key.toString()+";";
+				}
+				
+				 return result;
+
+			}
+			log.error("Element not found");
+			 return "ERROR";
+		}catch(Exception e){
+			log.fatal("Fatal "+e.getMessage());
+
+			 return "FATAL ERROR";
+
+		}
+	}
 
 }
