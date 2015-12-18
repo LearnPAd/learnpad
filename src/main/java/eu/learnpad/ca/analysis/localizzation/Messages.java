3313db8ec74faf17e48d425b8defe1561a316f9a
Giorgio <imatesiu@gmail.com>
2015-12-18 19:56:37 +0100

    it messages added

--- src/main/java/eu/learnpad/ca/analysis/localizzation/Messages.java
+++ src/main/java/eu/learnpad/ca/analysis/localizzation/Messages.java
@@ -1,21 +1,28 @@
 package eu.learnpad.ca.analysis.localizzation;
 
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 import org.languagetool.Language;
+import org.languagetool.language.Italian;
 
 public class Messages {
 	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle("localizzation/"+BUNDLE_NAME);
+	private static final ResourceBundle RESOURCE_BUNDLE_IT = ResourceBundle
+			.getBundle("localizzation/"+BUNDLE_NAME, new Locale("it"));
 
 	private Messages() {
 	}
 
 	public static String getString(String key, Language lang) {
 		try {
+			if(lang instanceof Italian){
+				return RESOURCE_BUNDLE_IT.getString(key);			
+			}
 			return RESOURCE_BUNDLE.getString(key);
 		} catch (MissingResourceException e) {
 			return '!' + key + '!';
