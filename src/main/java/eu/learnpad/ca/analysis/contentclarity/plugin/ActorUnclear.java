f8fbc4dc1293f922cd56905bf4f0313f6678006a
Giorgio <imatesiu@gmail.com>
2015-12-18 21:38:52 +0100

    ActorUnclear with lang Italian disabled

--- src/main/java/eu/learnpad/ca/analysis/contentclarity/plugin/ActorUnclear.java
+++ src/main/java/eu/learnpad/ca/analysis/contentclarity/plugin/ActorUnclear.java
@@ -12,29 +12,32 @@ import java.util.List;
 import java.util.Set;
 
 import org.languagetool.Language;
+import org.languagetool.language.Italian;
 
 public class ActorUnclear extends Plugin {
-	
+
 	protected static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ActorUnclear.class);
-	
+
 	public ActorUnclear(Language lang,  DocumentContent docContent, List<Node> listnode){
 		this.language=lang;
 		this.docContent = docContent;
 		this.listnode = listnode;
 	}
-	
+
 
 	public void check(GateThread gateu, List<Annotation> listannotations, Set<gate.Annotation> listSentenceDefected, Set<gate.Annotation> listSentence){
-		
-		HashSet<String> hs = new HashSet<String>();
-		hs.add("PassiveVoice"); //$NON-NLS-1$
-		Set<gate.Annotation> SetActorUnclear = gateu.getAnnotationSet(hs);
-		
-		
-		String rac = Messages.getString("ActorUnclear.Reccomandation", language); //$NON-NLS-1$
-		
-		String type = "Actor Unclear"; //$NON-NLS-1$
-		gatevsleanpadAnnotation(SetActorUnclear, listannotations,listSentenceDefected,listnode,docContent,type ,rac,log,listSentence );
+		if(!(language instanceof Italian)){
+			HashSet<String> hs = new HashSet<String>();
+			hs.add("PassiveVoice"); //$NON-NLS-1$
+			Set<gate.Annotation> SetActorUnclear = gateu.getAnnotationSet(hs);
+
+
+			String rac = Messages.getString("ActorUnclear.Reccomandation", language); //$NON-NLS-1$
+
+			String type = "Actor Unclear"; //$NON-NLS-1$
+			gatevsleanpadAnnotation(SetActorUnclear, listannotations,listSentenceDefected,listnode,docContent,type ,rac,log,listSentence );
+
+		}
 	}
 
 }
