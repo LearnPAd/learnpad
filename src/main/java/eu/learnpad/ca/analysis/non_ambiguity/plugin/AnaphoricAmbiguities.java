f1e39fa774526faf5540a697a6706cf370294436
Giorgio <imatesiu@gmail.com>
2015-12-18 19:24:22 +0100

    non_ambiguity string externalized

--- src/main/java/eu/learnpad/ca/analysis/non_ambiguity/plugin/AnaphoricAmbiguities.java
+++ src/main/java/eu/learnpad/ca/analysis/non_ambiguity/plugin/AnaphoricAmbiguities.java
@@ -1,4 +1,4 @@
-package eu.learnpad.ca.analysis.non_ambiguity.syntacticambiguity.plugin;
+package eu.learnpad.ca.analysis.non_ambiguity.plugin;
 
 import java.util.HashSet;
 import java.util.List;
@@ -7,6 +7,7 @@ import java.util.Set;
 import org.languagetool.Language;
 
 import eu.learnpad.ca.analysis.Plugin;
+import eu.learnpad.ca.analysis.localizzation.Messages;
 import eu.learnpad.ca.gate.GateThread;
 import eu.learnpad.ca.rest.data.Annotation;
 import eu.learnpad.ca.rest.data.Node;
@@ -26,14 +27,14 @@ public class AnaphoricAmbiguities extends Plugin {
 	public void check(GateThread gateu, List<Annotation> listannotations, Set<gate.Annotation> listSentenceDefected,Set<gate.Annotation> listSentence){
 		
 		HashSet<String> hs = new HashSet<String>();
-		hs.add("AnaphoricAmbiguity1");
-		hs.add("AnaphoricAmbiguity2");
+		hs.add("AnaphoricAmbiguity1"); //$NON-NLS-1$
+		hs.add("AnaphoricAmbiguity2"); //$NON-NLS-1$
 		Set<gate.Annotation> SetActorUnclear = gateu.getAnnotationSet(hs);
 		
 		
-		String rac = "The sentence is ambiguous because you are pronouns instead of names. Clarify the sentence by replacing the pronouns with names.";
+		String rac = Messages.getString("AnaphoricAmbiguities.Recomandation", language); //$NON-NLS-1$
 		
-		String type = "AnaphoricAmbiguities";
+		String type = "AnaphoricAmbiguities"; //$NON-NLS-1$
 		gatevsleanpadAnnotation(SetActorUnclear, listannotations,listSentenceDefected,listnode,docContent,type ,rac,log, listSentence );
 	}
 }
--- src/main/java/eu/learnpad/ca/analysis/non_ambiguity/plugin/CoordinationAmbiguity.java
+++ src/main/java/eu/learnpad/ca/analysis/non_ambiguity/plugin/CoordinationAmbiguity.java
@@ -1,4 +1,4 @@
-package eu.learnpad.ca.analysis.non_ambiguity.syntacticambiguity.plugin;
+package eu.learnpad.ca.analysis.non_ambiguity.plugin;
 
 import java.util.HashSet;
 import java.util.List;
@@ -7,16 +7,16 @@ import java.util.Set;
 import org.languagetool.Language;
 
 import eu.learnpad.ca.analysis.Plugin;
+import eu.learnpad.ca.analysis.localizzation.Messages;
 import eu.learnpad.ca.gate.GateThread;
 import eu.learnpad.ca.rest.data.Annotation;
 import eu.learnpad.ca.rest.data.Node;
 import gate.DocumentContent;
 
-public class PragmaticAmbiguity  extends Plugin {
+public class CoordinationAmbiguity extends Plugin {
+	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(CoordinationAmbiguity.class);
 
-/*	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(PragmaticAmbiguity.class);
-
-	public PragmaticAmbiguity(Language lang,  DocumentContent docContent, List<Node> listnode){
+	public CoordinationAmbiguity(Language lang,  DocumentContent docContent, List<Node> listnode){
 		this.language=lang;
 		this.docContent = docContent;
 		this.listnode = listnode;
@@ -26,12 +26,15 @@ public class PragmaticAmbiguity  extends Plugin {
 	public void check(GateThread gateu, List<Annotation> listannotations, Set<gate.Annotation> listSentenceDefected,Set<gate.Annotation> listSentence){
 		
 		HashSet<String> hs = new HashSet<String>();
-		hs.add("SpecifiedName");
+		hs.add("CoordAmbiguity"); //$NON-NLS-1$
 		Set<gate.Annotation> SetActorUnclear = gateu.getAnnotationSet(hs);
 		
 		
-		String rac = "The term %s is ambiguous. Please specify it more precisely.";
-		String type = "Pragmatic Ambiguity";
+		String rac = Messages.getString("CoordinationAmbiguity.Recomandation", language); //$NON-NLS-1$
+		
+		String type = "Coordination Ambiguity"; //$NON-NLS-1$
 		gatevsleanpadAnnotation(SetActorUnclear, listannotations,listSentenceDefected,listnode,docContent,type ,rac,log,listSentence );
-	}*/
+	}
+
+
 }
