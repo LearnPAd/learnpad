73ac9a57a134b454c435991b92d7d7a7acf0ba38
Jean SIMARD <woshilapin@tuziwo.info>
2015-12-28 16:43:24 +0100

    [cp] Move REST calls to RestResource

--- lp-core-platform/lp-cp-xwiki-implementation/src/main/java/eu/learnpad/core/impl/me/XwikiController.java
+++ lp-core-platform/lp-cp-xwiki-implementation/src/main/java/eu/learnpad/core/impl/me/XwikiController.java
@@ -132,98 +132,10 @@ public class XwikiController extends Controller implements XWikiRestComponent, I
 				RestResource.CORE_REPOSITORY_SPACE, modelSetId, attachmentName,
 				modelSetFile);
 	}
-/*
+	
 	@Override
-	public String startModelSetVerification(String modelSetId, String type,
-			String verification) throws LpRestExceptionImpl {
-		String verificationProcessId = UUID.randomUUID().toString();
-		mvResults.put(verificationProcessId, new MVResults(modelSetId, type));
-		return verificationProcessId;
-	}
-
-	@Override
-	public MVResults checkModelSetVerification(String verificationProcessId)
-			throws LpRestExceptionImpl {
-		MVResults result = mvResults.get(verificationProcessId);
-		if (result != null) {
-			MVResults toReturn = new MVResults(result);
-			if (result.getStatus().equals("inprogress")) {
-				mvResults.get(verificationProcessId).terminate();
-			} else {
-				mvResults.remove(verificationProcessId);
-			}
-			if (toReturn.getStatus().equals("finished")) {
-				// Notify CW about a new model set imported
-				HttpClient httpClient = RestResource.getClient();
-				String uri = String.format(
-						"%s/learnpad/cw/bridge/modelsetimported/%s",
-						RestResource.REST_URI, toReturn.getModelSetId());
-				PutMethod putMethod = new PutMethod(uri);
-				putMethod.addRequestHeader("Accept", "application/xml");
-				NameValuePair[] queryString = new NameValuePair[1];
-				queryString[0] = new NameValuePair("type", toReturn.getType());
-				putMethod.setQueryString(queryString);
-				try {
-					httpClient.executeMethod(putMethod);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				// Notify OR about a new model set imported
-				httpClient = RestResource.getClient();
-				uri = String.format(
-						"%s/learnpad/or/bridge/modelsetimported/%s",
-						RestResource.REST_URI, toReturn.getModelSetId());
-				PostMethod postMethod = new PostMethod(uri);
-				postMethod.addRequestHeader("Accept", "application/xml");
-				queryString = new NameValuePair[1];
-				queryString[0] = new NameValuePair("type", toReturn.getType());
-				postMethod.setQueryString(queryString);
-				try {
-					httpClient.executeMethod(postMethod);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			return toReturn;
-		} else {
-			return null;
-		}
-	}
-*/
-	@Override
-	public Feedbacks getFeedbacks(String modelSetId) throws LpRestExceptionImpl {
-		// Now send the package's path to the importer for XWiki
-		HttpClient httpClient = RestResource.getClient();
-		String uri = String.format("%s/learnpad/cw/bridge/%s/feedbacks",
-				RestResource.REST_URI, modelSetId);
-		GetMethod getMethod = new GetMethod(uri);
-		getMethod.addRequestHeader("Accept", "application/xml");
-
-		try {
-			httpClient.executeMethod(getMethod);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		InputStream feedbacksStream = null;
-		try {
-			feedbacksStream = getMethod.getResponseBodyAsStream();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Feedbacks feedbacks = null;
-		try {
-			JAXBContext jc = JAXBContext.newInstance(Feedbacks.class);
-			Unmarshaller unmarshaller = jc.createUnmarshaller();
-			feedbacks = (Feedbacks) unmarshaller.unmarshal(feedbacksStream);
-		} catch (JAXBException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return feedbacks;
+	public Feedbacks getFeedbacks(String modelSetId) throws LpRestException {
+		return this.cw.getFeedbacks(modelSetId);
 	}
 
     @Override
