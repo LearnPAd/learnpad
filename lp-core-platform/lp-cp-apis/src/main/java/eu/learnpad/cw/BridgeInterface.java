7fe3225a02758bcd54d17da488a33fe78f1da1e4
Gulyx <guglielmo.deangelis@isti.cnr.it>
2016-02-15 15:36:56 +0100

    Added Notificiation API for Recommandations on the Bridge interface of the CW

--- lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/cw/BridgeInterface.java
+++ lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/cw/BridgeInterface.java
@@ -27,9 +27,10 @@ import eu.learnpad.cw.rest.GetFeedbacks;
 import eu.learnpad.cw.rest.GetResource;
 import eu.learnpad.cw.rest.ModelSetImported;
 import eu.learnpad.cw.rest.ModelVerified;
+import eu.learnpad.cw.rest.NotifyRecommendations;
 
 @Path("/learnpad/cw/bridge")
 public interface BridgeInterface extends GetComments, GetFeedbacks,
-		GetResource, ModelSetImported, ContentVerified, ModelVerified {
+		GetResource, ModelSetImported, ContentVerified, ModelVerified, NotifyRecommendations {
 
 }
