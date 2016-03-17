f6313bfd10e3d5d77cd180d64c854e05196ad3db
Sandro Emmenegger <sandro.emmenegger@gmx.ch>
2016-03-17 10:14:50 +0100

    recommender fix for duplicate check

--- lp-ontology-recommender/src/main/java/eu/learnpad/ontology/recommender/Recommender.java
+++ lp-ontology-recommender/src/main/java/eu/learnpad/ontology/recommender/Recommender.java
@@ -8,7 +8,6 @@ package eu.learnpad.ontology.recommender;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
@@ -36,35 +35,35 @@ import eu.learnpad.or.rest.data.SimilarCases;
  * @author sandro.emmenegger
  */
 public class Recommender {
-    
+
     private static final Recommender instance = new Recommender();
-       
+
     public static final String QUERY_LINEMANAGER_AS_EXPERT = "linemanagerAsExpert";
     public static final String QUERY_EXPERTS_WITH_SAME_ROLE = "expertsWithSameRole";
     public static final String QUERY_EXPERT_MOST_OFTEN_EXECUTED_TASK = "expertMostOftenExecutedTask";
     public static final String QUERY_LEARNING_MATERIAL_FOR_NEXT_COMPETENCY_LEVEL = "learningMaterialForNextCompetencyLevel";
-    
-    
-    private Recommender() {}
-    
-    public static Recommender getInstance(){
+
+    private Recommender() {
+    }
+
+    public static Recommender getInstance() {
         return instance;
     }
-    
-    public Recommendations getRecommendations(String modelSetId, String artifactId, String userId, String simulationSessionId) throws RecommenderException{
+
+    public Recommendations getRecommendations(String modelSetId, String artifactId, String userId, String simulationSessionId) throws RecommenderException {
         ArgumentCheck.notNull(modelSetId, "modelsetId");
         ArgumentCheck.notNull(userId, "userId");
-        
+
         Recommendations recommends = getRecommendations(modelSetId, artifactId, userId);
-        if(simulationSessionId != null && !simulationSessionId.isEmpty() && !"--none--".equals(simulationSessionId)){
+        if (simulationSessionId != null && !simulationSessionId.isEmpty() && !"--none--".equals(simulationSessionId)) {
             SimilarCases similarCases = CBRAdapter.getInstance().retrieveSimilarCases(modelSetId, artifactId, userId, simulationSessionId);
             recommends.setSimilarCases(similarCases);
         }
-        
+
         return recommends;
     }
-    
-    public Recommendations getRecommendations(String modelSetId, String artifactId, String userId) throws RecommenderException{
+
+    public Recommendations getRecommendations(String modelSetId, String artifactId, String userId) throws RecommenderException {
         ArgumentCheck.notNull(modelSetId, "modelsetId");
         ArgumentCheck.notNull(userId, "userId");
 
@@ -74,28 +73,29 @@ public class Recommender {
         addBusinessActorsWithoutDuplicatesOrSelf(experts, suggestExpertsWithSameRole(modelSetId, artifactId, userId), userId);
         addBusinessActorsWithoutDuplicatesOrSelf(experts, suggestExpertMostOftenExecutedTask(modelSetId, artifactId, userId), userId);
         recommends.setExperts(experts);
-        
+
         LearningMaterials materials = new LearningMaterials();
         addLearningMaterialWithoutDuplicates(materials, getLearningMaterial(modelSetId, userId));
         recommends.setLearningMaterials(materials);
         return recommends;
     }
-    public List<BusinessActor> suggestLineManagerAsExpert(String modelSetId, String artifactId, String userId) throws RecommenderException{
+
+    public List<BusinessActor> suggestLineManagerAsExpert(String modelSetId, String artifactId, String userId) throws RecommenderException {
         return getExperts(QUERY_LINEMANAGER_AS_EXPERT, modelSetId, artifactId, userId);
-    }    
-    
-    public List<BusinessActor> suggestExpertsWithSameRole(String modelSetId, String artifactId, String userId) throws RecommenderException{
+    }
+
+    public List<BusinessActor> suggestExpertsWithSameRole(String modelSetId, String artifactId, String userId) throws RecommenderException {
         return getExperts(QUERY_EXPERTS_WITH_SAME_ROLE, modelSetId, artifactId, userId);
     }
-    
-    public List<BusinessActor> suggestExpertMostOftenExecutedTask(String modelSetId, String artifactId, String userId) throws RecommenderException{
-        if(artifactId == null){
+
+    public List<BusinessActor> suggestExpertMostOftenExecutedTask(String modelSetId, String artifactId, String userId) throws RecommenderException {
+        if (artifactId == null) {
             return new ArrayList<>();
         }
         return getExperts(QUERY_EXPERT_MOST_OFTEN_EXECUTED_TASK, modelSetId, artifactId, userId);
     }
 
-    private List<BusinessActor> getExperts(String queryName, String modelSetId, String artifactId, String userId) throws RecommenderException{
+    private List<BusinessActor> getExperts(String queryName, String modelSetId, String artifactId, String userId) throws RecommenderException {
         RecommenderQuery queryObj = QueryMap.getQuery(queryName);
         List<BusinessActor> experts = new ArrayList<>();
         Query query = QueryFactory.create(queryObj.getQueryString());
@@ -104,10 +104,10 @@ public class Recommender {
         QueryExecution qexec = null;
         QuerySolutionMap initialBinding = new QuerySolutionMap();
         initialBinding.add("userId", model.createTypedLiteral(userId));
-        if(artifactId != null){
-           initialBinding.add("artifactId", model.getResource(APP.NS.TRANSFER+artifactId));
+        if (artifactId != null) {
+            initialBinding.add("artifactId", model.getResource(APP.NS.TRANSFER + artifactId));
         }
-        
+
         try {
             qexec = QueryExecutionFactory.create(query, model, initialBinding);
             ResultSet results = qexec.execSelect();
@@ -133,21 +133,21 @@ public class Recommender {
 
     public OntModel getExecutionDataModel(String modelSetId) throws RecommenderException {
         OntModel model = FileOntAO.getInstance().getExecutionData(modelSetId);
-        if(model == null){
-            throw new RecommenderException("Ontology model for modelset with id '"+modelSetId+"' cannot be found or causes problems during loading.");
+        if (model == null) {
+            throw new RecommenderException("Ontology model for modelset with id '" + modelSetId + "' cannot be found or causes problems during loading.");
         }
         return model;
     }
-    
-    private String getLiteralString(QuerySolution soln, String varName){
+
+    private String getLiteralString(QuerySolution soln, String varName) {
         Literal lit = soln.getLiteral(varName);
-        if(lit != null){
+        if (lit != null) {
             return lit.getString();
         }
         return null;
     }
 
-    private List<LearningMaterial>  getLearningMaterial(String modelSetId, String userId) throws RecommenderException {
+    private List<LearningMaterial> getLearningMaterial(String modelSetId, String userId) throws RecommenderException {
         RecommenderQuery queryObj = QueryMap.getQuery(QUERY_LEARNING_MATERIAL_FOR_NEXT_COMPETENCY_LEVEL);
         List<LearningMaterial> materials = new ArrayList<>();
         Query query = QueryFactory.create(queryObj.getQueryString());
@@ -155,7 +155,7 @@ public class Recommender {
         QueryExecution qexec = null;
         QuerySolutionMap initialBinding = new QuerySolutionMap();
         initialBinding.add("userId", model.createTypedLiteral(userId));
-        
+
         try {
             qexec = QueryExecutionFactory.create(query, model, initialBinding);
             ResultSet results = qexec.execSelect();
@@ -179,49 +179,50 @@ public class Recommender {
         }
         return materials;
     }
-    
-    private void addBusinessActorsWithoutDuplicatesOrSelf(Experts experts, List<BusinessActor> toAdd, String userId){
-        
+
+    private void addBusinessActorsWithoutDuplicatesOrSelf(Experts experts, List<BusinessActor> toAdd, String userId) {
+
         List<BusinessActor> businessActors = experts.getBusinessActors();
-        if(businessActors == null){
+        if (businessActors == null) {
             businessActors = new ArrayList();
-            for (BusinessActor baToAdd : toAdd) {
-                if(userId != null && userId.equals(baToAdd.getEmail())){
-                    continue;
-                }
-                boolean exists = false;
-                for (BusinessActor businessActor : businessActors) {
-                    if(businessActor.getUri().equals(baToAdd.getUri())){
-                        exists = true;
-                        break;
-                    }
-                }
-                if(!exists){
-                    businessActors.add(baToAdd);
+        }
+        for (BusinessActor baToAdd : toAdd) {
+            if (userId != null && userId.equals(baToAdd.getEmail())) {
+                continue;
+            }
+            boolean exists = false;
+            for (BusinessActor businessActor : businessActors) {
+                if (businessActor.getUri().equals(baToAdd.getUri())) {
+                    exists = true;
+                    break;
                 }
             }
+            if (!exists) {
+                businessActors.add(baToAdd);
+            }
         }
+
         experts.setBusinessActors(businessActors);
     }
-    
-    private void addLearningMaterialWithoutDuplicates(LearningMaterials materials, List<LearningMaterial> toAdd){
-        
+
+    private void addLearningMaterialWithoutDuplicates(LearningMaterials materials, List<LearningMaterial> toAdd) {
+
         List<LearningMaterial> materialsList = materials.getLearningMaterials();
-        if(materialsList == null){
+        if (materialsList == null) {
             materialsList = new ArrayList();
             for (LearningMaterial materialToAdd : toAdd) {
                 boolean exists = false;
                 for (LearningMaterial learningMaterial : materialsList) {
-                    if(learningMaterial.getUrl().equals(materialToAdd.getUrl())){
+                    if (learningMaterial.getUrl().equals(materialToAdd.getUrl())) {
                         exists = true;
                         break;
                     }
                 }
-                if(!exists){
+                if (!exists) {
                     materialsList.add(materialToAdd);
                 }
             }
         }
         materials.setLearningMaterials(materialsList);
-    }    
+    }
 }
