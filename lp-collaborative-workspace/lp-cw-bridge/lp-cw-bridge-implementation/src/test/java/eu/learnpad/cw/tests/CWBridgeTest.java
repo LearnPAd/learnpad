24b8b6c378b24477cc5e8cbcfa46615271f80a37
Gulyx <guglielmo.deangelis@isti.cnr.it>
2016-04-15 10:12:03 +0200

    added support for mockito

--- lp-collaborative-workspace/lp-cw-bridge/lp-cw-bridge-implementation/src/test/java/eu/learnpad/cw/tests/CWBridgeTest.java
+++ lp-collaborative-workspace/lp-cw-bridge/lp-cw-bridge-implementation/src/test/java/eu/learnpad/cw/tests/CWBridgeTest.java
@@ -6,7 +6,9 @@ import java.util.Map;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.xwiki.component.manager.ComponentLookupException;
 import org.xwiki.component.phase.InitializationException;
+import org.xwiki.test.mockito.MockitoComponentMockingRule;
 
 import eu.learnpad.cw.CWXwikiBridge;
 import eu.learnpad.exception.LpRestException;
@@ -25,9 +27,12 @@ public class CWBridgeTest {
 		this.random = new SecureRandom();
 	}
 		
+	 @Rule
+     public final MockitoComponentMockingRule<CWXwikiBridge> mocker = new MockitoComponentMockingRule(CWXwikiBridge.class);
+	
 	@Test
-	public void testInsertNotifiedReccomandations(){	
-		bridge = new CWXwikiBridge();
+	public void testInsertNotifiedReccomandations() throws ComponentLookupException, InitializationException, LpRestException{	
+		bridge = mocker.getComponentUnderTest();
 		
 		int maxTentatives = this.random.nextInt(4);
 		long baseValue = this.random.nextLong()/2;
@@ -36,32 +41,22 @@ public class CWBridgeTest {
 		String modelSetId = "mod."+String.valueOf(this.random.nextInt());
 		String userId = "dummyUser";
 		
-		try {
-			bridge.initialize();
+//		bridge.initialize();
 			
-			for (int i=0; i<maxTentatives; i++){
-				baseValue += 1;
+		for (int i=0; i<maxTentatives; i++){
+			baseValue += 1;
 // All the recs belongs to a different simulation session			
-				String simulationid = String.valueOf(baseValue);
-				bridge.notifyRecommendations(modelSetId, simulationid, userId, rec);				
-			}
-			Map<String,Recommendations> notifiedRecsMaps = bridge.getNotifiedRecommendations(userId);
-// All the recs must be preserved			
-			Assert.assertTrue( notifiedRecsMaps.size() == maxTentatives );
-			
-		} catch (InitializationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (LpRestException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			String simulationid = String.valueOf(baseValue);
+			bridge.notifyRecommendations(modelSetId, simulationid, userId, rec);				
 		}
-		Assert.assertFalse(true);		
+		Map<String,Recommendations> notifiedRecsMaps = bridge.getNotifiedRecommendations(userId);
+// All the recs must be preserved			
+		Assert.assertTrue( notifiedRecsMaps.size() == maxTentatives );
 	}
 	
 	@Test
-	public void testBannedSimIdInNotifiedReccomandations(){	
-		bridge = new CWXwikiBridge();
+	public void testBannedSimIdInNotifiedReccomandations() throws ComponentLookupException, InitializationException, LpRestException{	
+		bridge = mocker.getComponentUnderTest();
 				
 		int maxTentatives = this.random.nextInt(4)+1;
 // All the recs belongs to a the same simulation session			
@@ -71,29 +66,19 @@ public class CWBridgeTest {
 		String modelSetId = "mod."+String.valueOf(this.random.nextInt());
 		String userId = "dummyUser";
 		
-		try {
-			bridge.initialize();
+//		bridge.initialize();
 			
-			bridge.notifyRecommendations(modelSetId, simulationid, userId, rec);				
+		bridge.notifyRecommendations(modelSetId, simulationid, userId, rec);				
 // Now the rec is deleted and the simulationid become banned			
-			bridge.deleteRecommendations(modelSetId, simulationid, userId);
+		bridge.deleteRecommendations(modelSetId, simulationid, userId);
 			
-			for (int i=0; i<maxTentatives; i++){
+		for (int i=0; i<maxTentatives; i++){
 // Since the simulationid is banned, so no more recs for this simulation session are accepted
-				bridge.notifyRecommendations(modelSetId, simulationid, userId, rec);				
-			}
-			
-			Map<String,Recommendations> notifiedRecsMaps = bridge.getNotifiedRecommendations(userId);
-			Assert.assertTrue( notifiedRecsMaps.isEmpty() );
-			
-		} catch (InitializationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (LpRestException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			bridge.notifyRecommendations(modelSetId, simulationid, userId, rec);				
 		}
-		Assert.assertFalse(true);		
+			
+		Map<String,Recommendations> notifiedRecsMaps = bridge.getNotifiedRecommendations(userId);
+		Assert.assertTrue( notifiedRecsMaps.isEmpty() );			
 	}
 	
 }
