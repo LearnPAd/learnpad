6da040fa26fe7eec27954f5423fda182a779608b
acalabro <antonello.calabro@isti.cnr.it>
2016-05-31 13:15:54 +0200

    The ScoreUpdateEvent is now created using the ENUM ScoreType to define the kind of score that is representing

--- lp-simulation-environment/monitoring/src/main/java/eu/learnpad/simulator/mon/event/ScoreUpdateEvent.java
+++ lp-simulation-environment/monitoring/src/main/java/eu/learnpad/simulator/mon/event/ScoreUpdateEvent.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package eu.learnpad.sim.rest.event.impl;
+package eu.learnpad.simulator.mon.event;
 
 import java.util.List;
 import java.util.Map;
