359215d58c3b53298fcb090b1762646994b5dd69
Gulyx <guglielmo.deangelis@isti.cnr.it>
2016-07-30 11:50:32 +0200

    Improved declaration of the enumerations toward a better compatibility with JAXB

--- lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/or/rest/data/NotificationActionType.java
+++ lp-core-platform/lp-cp-apis/src/main/java/eu/learnpad/or/rest/data/NotificationActionType.java
@@ -4,12 +4,20 @@ import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlType;
 
 /**
- * All possible actions of notification events. 
+ * All possible actions of notification events.
  * 
  * @author sandro.emmenegger
  */
-@XmlType
+@XmlType(name = "NotificationActionType")
 @XmlEnum
 public enum NotificationActionType {
-	VISITED, ADDED, MODIFIED, DELETED
+	VISITED, ADDED, MODIFIED, DELETED;
+
+	public String value() {
+		return name();
+	}
+
+	public static NotificationActionType fromValue(String v) {
+		return valueOf(v);
+	}
 }
