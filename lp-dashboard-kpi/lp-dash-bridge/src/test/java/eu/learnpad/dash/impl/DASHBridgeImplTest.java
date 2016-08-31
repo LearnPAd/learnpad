ec201d9444d3c0d429f5dbeb2a0e8408c9dde7b8
Gulyx <guglielmo.deangelis@isti.cnr.it>
2016-08-31 12:32:12 +0200

    Improved the way the lp-dash-bridge uses internal/external configuration files (i.e. property files)

--- lp-dashboard-kpi/lp-dash-bridge/src/test/java/eu/learnpad/dash/impl/DASHBridgeImplTest.java
+++ lp-dashboard-kpi/lp-dash-bridge/src/test/java/eu/learnpad/dash/impl/DASHBridgeImplTest.java
@@ -6,9 +6,13 @@
 package eu.learnpad.dash.impl;
 
 import eu.learnpad.dash.config.APP;
+import eu.learnpad.dash.config.PropertyUtil;
+
 import java.io.File;
 import java.io.InputStream;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -20,19 +24,25 @@ public class DASHBridgeImplTest {
     protected static final String MODELSET_ID = APP.CONF.getString("testdata.modelset.version");
     protected static final String TEST_USER = "barnaby.barnes@learnpad.com";
 
+    private PropertyUtil conf;
+
     public DASHBridgeImplTest() {
+    	this.conf=new PropertyUtil();
     }
 
     @Test
     public void testLoadKPIValues() throws Exception {
-        File baseWorkingDirectory = APP.getWorkingDirectory();
+//        File baseWorkingDirectory = APP.getWorkingDirectory();
+        File baseWorkingDirectory = this.conf.getWorkingDirectory();
         assertNotNull(baseWorkingDirectory);
-        File dashobardFilesDir = new File(baseWorkingDirectory + "/" + APP.CONF.getString("lp-dash.kpi.dashboard.data.folder.relative"));
+        File dashobardFilesDir = new File(baseWorkingDirectory + "/" + this.conf.getProperty("lp-dash.kpi.dashboard.data.folder.relative"));
+//        File dashobardFilesDir = new File(baseWorkingDirectory + "/" + APP.CONF.getString("lp-dash.kpi.dashboard.data.folder.relative"));
         if(dashobardFilesDir.exists()){
             deleteFiles(dashobardFilesDir);
         }
         
-        InputStream cockpitContent = DASHBridgeImplTest.class.getResourceAsStream(APP.CONF.getString("testdata.kpi.input.file"));
+        InputStream cockpitContent = DASHBridgeImplTest.class.getResourceAsStream(this.conf.getProperty("testdata.kpi.input.file"));
+//        InputStream cockpitContent = DASHBridgeImplTest.class.getResourceAsStream(APP.CONF.getString("testdata.kpi.input.file"));
         assertNotNull(cockpitContent);
         DASHBridgeImpl instance = new DASHBridgeImpl();
         instance.loadKPIValues(MODELSET_ID, null, TEST_USER, cockpitContent);
@@ -45,7 +55,8 @@ public class DASHBridgeImplTest {
     public void testGetKPIValuesView() throws Exception {
 
         DASHBridgeImpl instance = new DASHBridgeImpl();
-        String expResult = APP.CONF.getString("lp-dash.cockpit.url") + "?businessActorId=" + TEST_USER;
+        String expResult = this.conf.getProperty("lp-dash.cockpit.url") + "?businessActorId=" + TEST_USER;
+//        String expResult = APP.CONF.getString("lp-dash.cockpit.url") + "?businessActorId=" + TEST_USER;
         String result = instance.getKPIValuesView(MODELSET_ID, TEST_USER);
         assertEquals(expResult, result);
     }
