fad2ce7fbb6a288e722d4953b29bb97ec93f45e3
acalabro <antonello.calabro@isti.cnr.it>
2016-09-07 16:00:53 +0200

    min up

--- lp-simulation-environment/monitoring/src/main/java/eu/learnpad/simulator/mon/impl/PathExplorerImpl.java
+++ lp-simulation-environment/monitoring/src/main/java/eu/learnpad/simulator/mon/impl/PathExplorerImpl.java
@@ -27,23 +27,23 @@ public class PathExplorerImpl implements PathExplorer {
 		kpiExample.put("kpiThree", 0.3f);
 		kpiExample.put("kpiFour", 0.4f);
 		
-		Activity serviceConfInv = new Activity("Service Conference Invitation","Service Conference Invitation", "taskArtifactID1", kpiExample, 2.0f);
-		Activity putStamps = new Activity("Putting Stamps on the received documentation", "Putting Stamps on the received documentation", "taskArtifactID2",kpiExample, 2.0f);
-		Activity checkProvided = new Activity("Check Provided Documentation","Check Provided Documentation","taskArtifactID3", kpiExample, 2.0f);
-		Activity takeADecision = new Activity("Take a decision","Take a decision","taskArtifactID4", kpiExample, 2.0f);	
-		Activity provideOp = new Activity("Provide Opinion","Provide Opinion", "taskArtifactID5",kpiExample, 2.0f);
-		Activity receivedOpinion = new Activity("Received Opinion Evaluation","Received Opinion Evaluation","taskArtifactID6", kpiExample, 2.0f);
-		Activity provideAuthUnder = new Activity("Provide Authorization Under Condition","Provide Authorization Under Condition","taskArtifactID7", kpiExample, 2.0f);
-		Activity provideAuth = new Activity("Provide Authorization","Provide Authorization","taskArtifactID8", kpiExample, 2.0f);
-		Activity provideFinal = new Activity("Provide Final Report","Provide Final Report","taskArtifactID9", kpiExample, 2.0f);
+		Activity serviceConfInv = new Activity("Service Conference Invitation","Service Conference Invitation", "usertask1", kpiExample, 1.0f);
+		Activity putStamps = new Activity("Putting Stamps on the received documentation", "Putting Stamps on the received documentation", "usertask2",kpiExample, 1.0f);
+		Activity checkProvided = new Activity("Check Provided Documentation","Check Provided Documentation","usertask4", kpiExample, 1.0f);
+		Activity takeADecision = new Activity("Take a decision","Take a decision","usertask8", kpiExample, 1.0f);	
+		Activity provideOp = new Activity("Provide Opinion","Provide Opinion", "usertask14",kpiExample, 1.0f);
+		Activity receivedOpinion = new Activity("Received Opinion Evaluation","Received Opinion Evaluation","taskArtifactID6", kpiExample, 1.0f);
+		Activity provideAuthUnder = new Activity("Provide Authorization Under Condition","Provide Authorization Under Condition","usertask19", kpiExample, 1.0f);
+		Activity provideAuth = new Activity("Provide Authorization","Provide Authorization","usertask18", kpiExample, 1.0f);
+		Activity provideFinal = new Activity("Provide Final Report","Provide Final Report","usertask22", kpiExample, 1.0f);
 		
 		lastExploredBPMN.add(new Activity[]
 						{serviceConfInv});
-		lastExploredBPMN.add(new Activity[]
-						{serviceConfInv, putStamps, checkProvided, takeADecision, provideOp, receivedOpinion, provideAuthUnder, provideFinal});
+//		lastExploredBPMN.add(new Activity[]
+//						{serviceConfInv, putStamps, checkProvided, takeADecision, provideOp, receivedOpinion, provideAuthUnder, provideFinal});
 
 		lastExploredBPMN.add(new Activity[]
-						{serviceConfInv, putStamps, checkProvided, takeADecision, provideOp, receivedOpinion, provideAuth, provideFinal});
+						{serviceConfInv, putStamps, checkProvided, takeADecision, provideOp, provideAuth, provideFinal});
 		//END FAKE EXPLORATION
 		
 		return lastExploredBPMN;
