578541e58c2d3c72920218e1a2950e500574456d
Gulyx <guglielmo.deangelis@isti.cnr.it>
2016-09-07 12:21:09 +0200

    Improved test

--- lp-core-platform/lp-cp-xwiki-implementation/src/test/java/eu/learnpad/core/impl/or/tests/XwikiBridgeInterfaceRestResourceTest.java
+++ lp-core-platform/lp-cp-xwiki-implementation/src/test/java/eu/learnpad/core/impl/or/tests/XwikiBridgeInterfaceRestResourceTest.java
@@ -1,5 +1,8 @@
 package eu.learnpad.core.impl.or.tests;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -9,6 +12,7 @@ import eu.learnpad.core.impl.tests.AbstractUnitTest;
 import eu.learnpad.exception.LpRestException;
 import eu.learnpad.or.rest.data.NotificationActionType;
 import eu.learnpad.or.rest.data.ResourceType;
+import eu.learnpad.sim.rest.event.ScoreType;
 
 public class XwikiBridgeInterfaceRestResourceTest extends AbstractUnitTest {
 	private XwikiBridgeInterfaceRestResource bridge;
@@ -56,7 +60,7 @@ public class XwikiBridgeInterfaceRestResourceTest extends AbstractUnitTest {
 			String referringToResourceId = "foo";
 			String modelId = "fooModelId";
 			String artifactId = "fooArtifactId";
-			String userId = "bbarnes@learnpad.eu";
+			String userId = "b.barnes@learnpad.eu";
 			NotificationActionType action = NotificationActionType.ADDED;
 			ResourceType resourceType = ResourceType.PAGE;
 			String modelSetId = "fooModelSetId";
@@ -66,11 +70,43 @@ public class XwikiBridgeInterfaceRestResourceTest extends AbstractUnitTest {
 		}
 		Assert.assertFalse(exceptionRaised);
 	}
+	
+	@Test
+	@Ignore	
+	public void updateSimulationScoreTest(){
+		String modelSetId = "modelsetid";
+		String simulationSessionId = "simulationsessionid";
+		String processArtifactId = "processartifactid";
+
+		Long timestamp = System.currentTimeMillis();
+
+		String userId = "b.barnes@learnpad.eu";
+
+		Map<String, Object> simulationSessionData = new HashMap<String, Object>();
+		for (int i = this.getRandomGenerator().nextInt(10); i >= 0; i--) {
+			simulationSessionData.put("data" + i, "value" + i);
+		}
+
+		for (ScoreType scoreType : ScoreType.values()) {
+			Float scoreValue = this.getRandomGenerator().nextFloat();
+
+			try {
+				this.bridge.updateSimulationScore(modelSetId, simulationSessionId, processArtifactId, timestamp, userId, scoreType, scoreValue);
+			} catch (LpRestException e) {
+				System.err.println(e.getMessage());
+				Assert.assertFalse(true);
+			}
+		}
+
+		Assert.assertFalse(false);
+	}
+
 
 	public static void main (String[] args){
 		XwikiBridgeInterfaceRestResourceTest me = new XwikiBridgeInterfaceRestResourceTest();		
 //		me.calculateKPITest();
 //		me.getHandlingProcessStatusTest();
-		me.resourceNotificationTest();
+//		me.resourceNotificationTest();
+		me.updateSimulationScoreTest();
 	}
 }
