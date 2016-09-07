28ef6783ff55eb5728ff22f340920d9ef2828a36
acalabro <antonello.calabro@isti.cnr.it>
2016-09-07 16:57:26 +0200

    up

--- lp-simulation-environment/monitoring/src/main/java/eu/learnpad/simulator/mon/impl/PathExplorerImpl.java
+++ lp-simulation-environment/monitoring/src/main/java/eu/learnpad/simulator/mon/impl/PathExplorerImpl.java
@@ -27,23 +27,40 @@ public class PathExplorerImpl implements PathExplorer {
 		kpiExample.put("kpiThree", 0.3f);
 		kpiExample.put("kpiFour", 0.4f);
 		
-		Activity serviceConfInv = new Activity("Service Conference Invitation","Service Conference Invitation", "usertask1", kpiExample, 1.0f);
-		Activity putStamps = new Activity("Putting Stamps on the received documentation", "Putting Stamps on the received documentation", "usertask2",kpiExample, 1.0f);
-		Activity checkProvided = new Activity("Check Provided Documentation","Check Provided Documentation","usertask4", kpiExample, 1.0f);
-		Activity takeADecision = new Activity("Take a decision","Take a decision","usertask8", kpiExample, 1.0f);	
-		Activity provideOp = new Activity("Provide Opinion","Provide Opinion", "usertask14",kpiExample, 1.0f);
-		Activity receivedOpinion = new Activity("Received Opinion Evaluation","Received Opinion Evaluation","taskArtifactID6", kpiExample, 1.0f);
-		Activity provideAuthUnder = new Activity("Provide Authorization Under Condition","Provide Authorization Under Condition","usertask19", kpiExample, 1.0f);
-		Activity provideAuth = new Activity("Provide Authorization","Provide Authorization","usertask18", kpiExample, 1.0f);
-		Activity provideFinal = new Activity("Provide Final Report","Provide Final Report","usertask22", kpiExample, 1.0f);
+		Activity assessApplication = new Activity("Assess Application", "Assess Application", "obj.27830", kpiExample, 1.0f);
+		Activity sendNotAmmissible = new Activity("Send Communication of non-admissible instance", "Send Communication of non-admissible instance", "obj.27812", kpiExample, 1.0f);
+		Activity checkApplication = new Activity("Check Application", "Check Application", "obj.27782", kpiExample, 1.0f);
+		Activity requestAmendment = new Activity("Request Amendment", "Request Amendment", "obj.29013", kpiExample, 1.0f);
+		Activity checkAmendment = new Activity("Check Amendment", "Check Amendment", "obj.27833", kpiExample, 1.0f);	
+		Activity sendAuthorization = new Activity("Send Authorization Document", "Send Authorization Document", "obj.27788", kpiExample, 1.0f);
+		Activity manageInhibition = new Activity("Manage Inhibition", "Manage Inhibition", "obj.27839", kpiExample, 1.0f);
+		
+		
+//		Activity serviceConfInv = new Activity("Service Conference Invitation","Service Conference Invitation", "usertask1", kpiExample, 1.0f);
+//		Activity putStamps = new Activity("Putting Stamps on the received documentation", "Putting Stamps on the received documentation", "usertask2",kpiExample, 1.0f);
+//		Activity checkProvided = new Activity("Check Provided Documentation","Check Provided Documentation","usertask4", kpiExample, 1.0f);
+//		Activity takeADecision = new Activity("Take a decision","Take a decision","usertask8", kpiExample, 1.0f);	
+//		Activity provideOp = new Activity("Provide Opinion","Provide Opinion", "usertask14",kpiExample, 1.0f);
+//		Activity receivedOpinion = new Activity("Received Opinion Evaluation","Received Opinion Evaluation","taskArtifactID6", kpiExample, 1.0f);
+//		Activity provideAuthUnder = new Activity("Provide Authorization Under Condition","Provide Authorization Under Condition","usertask19", kpiExample, 1.0f);
+//		Activity provideAuth = new Activity("Provide Authorization","Provide Authorization","usertask18", kpiExample, 1.0f);
+//		Activity provideFinal = new Activity("Provide Final Report","Provide Final Report","usertask22", kpiExample, 1.0f);
 		
 		lastExploredBPMN.add(new Activity[]
-						{serviceConfInv});
+						{assessApplication, sendNotAmmissible});
+		
+		lastExploredBPMN.add(new Activity[]
+				{assessApplication, checkApplication, requestAmendment, checkAmendment, sendAuthorization});
+		
+
+		lastExploredBPMN.add(new Activity[]
+				{assessApplication, checkApplication, requestAmendment, checkAmendment, manageInhibition});
+		
 //		lastExploredBPMN.add(new Activity[]
 //						{serviceConfInv, putStamps, checkProvided, takeADecision, provideOp, receivedOpinion, provideAuthUnder, provideFinal});
 
-		lastExploredBPMN.add(new Activity[]
-						{serviceConfInv, putStamps, checkProvided, takeADecision, provideOp, provideAuth, provideFinal});
+//		lastExploredBPMN.add(new Activity[]
+//						{serviceConfInv, putStamps, checkProvided, takeADecision, provideOp, provideAuth, provideFinal});
 		//END FAKE EXPLORATION
 		
 		return lastExploredBPMN;
